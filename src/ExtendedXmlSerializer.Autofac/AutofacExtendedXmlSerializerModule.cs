4988155f893eb9915f5cd273eaf4d4441b5345e4
wojtpl2 <wojtpl2@gmail.com>
2016-08-11 13:17:26 +0200

    Integration with autofac

---
+++ src/ExtendedXmlSerializer.Autofac/AutofacExtendedXmlSerializerModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+
+namespace ExtendedXmlSerialization.Autofac
+{
+    public class AutofacExtendedXmlSerializerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AutofacSerializationToolsFactory>()
+                .AsSelf()
+                .As<ISerializationToolsFactory>()
+                .SingleInstance();
+
+            builder.RegisterType<ExtendedXmlSerializer>().UsingConstructor(typeof(ISerializationToolsFactory))
+                .AsSelf()
+                .As<IExtendedXmlSerializer>()
+                .SingleInstance();
+        }
+    }
+}
---
+++ src/ExtendedXmlSerializer.Autofac/AutofacSerializationToolsFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Autofac;
+
+namespace ExtendedXmlSerialization.Autofac
+{
+    public class AutofacSerializationToolsFactory : ISerializationToolsFactory
+    {
+        private readonly IComponentContext _context;
+
+        public AutofacSerializationToolsFactory(IComponentContext context)
+        {
+            _context = context;
+        }
+        public IMigrationMap GetMigrationMap(Type type)
+        {
+            var genericTyep = typeof(IMigrationMap<>).MakeGenericType(type);
+            object result;
+            if (_context.TryResolve(genericTyep, out result))
+            {
+                return result as IMigrationMap;
+            }
+            return null;
+        }
+
+        public ICustomSerializator GetCustomSerializer(Type type)
+        {
+            var genericTyep = typeof(ICustomSerializator<>).MakeGenericType(type);
+            object result;
+            if (_context.TryResolve(genericTyep, out result))
+            {
+                return result as ICustomSerializator;
+            }
+            return null;
+        }
+    }
+}
