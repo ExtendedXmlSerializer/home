4c41f1359add66f8d45756ec0a43c71c0f690174
Mike-EEE <michael@dragonspark.us>
2017-08-12 02:44:42 -0400

    Updating runtime serializer to check during reader, for #116.

--- src/ExtendedXmlSerializer/ContentModel/RuntimeSerializer.cs
+++ src/ExtendedXmlSerializer/ContentModel/RuntimeSerializer.cs
@@ -1,18 +1,18 @@
 // MIT License
-// 
+//
 // Copyright (c) 2016 Wojciech Nagórski
 //                    Michael DeMond
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,15 +44,21 @@ namespace ExtendedXmlSerializer.ContentModel
 		public void Write(IFormatWriter writer, object instance)
 		{
 			var typeInfo = instance.GetType().GetTypeInfo();
+			var serializer = Serializer(typeInfo);
+			serializer.Write(writer, instance);
+		}
+
+		ISerializer Serializer(TypeInfo typeInfo)
+		{
 			var serializer = _contents.Get(typeInfo);
 			if (serializer is RuntimeSerializer)
 			{
 				throw new InvalidOperationException(
-					$"The serializer for type '{typeInfo}' could not be found.  Please ensure that the type is a valid type can be activated.");
+				                                    $"The serializer for type '{typeInfo}' could not be found.  Please ensure that the type is a valid type can be activated.");
 			}
-			serializer.Write(writer, instance);
+			return serializer;
 		}
 
-		public object Get(IFormatReader reader) => _contents.Get(_classification.Get(reader)).Get(reader);
+		public object Get(IFormatReader reader) => Serializer(_classification.Get(reader)).Get(reader);
 	}
 }
