74f0506a07115f98cadb1cbdebf98132dd496bd4
Mike-EEE <michael@dragonspark.us>
2017-10-03 11:49:56 -0400

    Addressing issue #129.

--- src/ExtendedXmlSerializer/ExtensionModel/References/ReferenceAwareSerializers.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/References/ReferenceAwareSerializers.cs
@@ -1,18 +1,18 @@
 // MIT License
-// 
+//
 // Copyright (c) 2016 Wojciech Nagórski
 //                    Michael DeMond
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,13 +21,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
-using System.Linq;
-using System.Reflection;
 using ExtendedXmlSerializer.ContentModel;
 using ExtendedXmlSerializer.ContentModel.Content;
 using ExtendedXmlSerializer.ContentModel.Format;
+using ExtendedXmlSerializer.Core;
 using ExtendedXmlSerializer.Core.Sources;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace ExtendedXmlSerializer.ExtensionModel.References
 {
@@ -68,7 +69,7 @@ namespace ExtendedXmlSerializer.ExtensionModel.References
 			public void Write(IFormatWriter writer, object instance)
 			{
 				var references = _references.Get(writer);
-				if (references.Any())
+				if (references.Any() && Conditions.Default.Get(writer).Apply()) // TODO: Might find a better way of handling this: https://github.com/wojtpl2/ExtendedXmlSerializer/issues/129
 				{
 					var typeInfo = instance.GetType()
 					                       .GetTypeInfo();
@@ -77,11 +78,13 @@ namespace ExtendedXmlSerializer.ExtensionModel.References
 						$"{line}{line}Here is a list of found references:{line}{string.Join(line, references.Select(x => $"- {x}"))}";
 
 					throw new CircularReferencesDetectedException(
-					                                              $"The provided instance of type '{typeInfo}' contains circular references within its graph. Serializing this instance would result in a recursive, endless loop. To properly serialize this instance, please create a serializer that has referential support enabled by extending it with the ReferencesExtension.{message}",
-					                                              _container);
+						$"The provided instance of type '{typeInfo}' contains circular references within its graph. Serializing this instance would result in a recursive, endless loop. To properly serialize this instance, please create a serializer that has referential support enabled by extending it with the ReferencesExtension.{message}",
+						_container);
 				}
 				_container.Write(writer, instance);
 			}
+
+
 		}
 	}
 }
