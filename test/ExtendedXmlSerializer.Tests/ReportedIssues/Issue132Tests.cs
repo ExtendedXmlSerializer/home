5f4bd81d064bd16a3a7bdbb780dea4ad4ecc7ce1
Mike-EEE <michael@dragonspark.us>
2017-11-06 02:35:04 -0500

    Merging tests.

--- test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue132Tests.cs
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue132Tests.cs
@@ -1,9 +1,16 @@
 ﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.ExtensionModel.Encryption;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using System.Xml.Linq;
+using XmlWriter = System.Xml.XmlWriter;
 
 // ReSharper disable All
 
 namespace ExtendedXmlSerializer.Tests.ReportedIssues
 {
+	using System;
+
 	public sealed class Issue132Tests
 	{
 		public void Examples()
@@ -15,15 +22,123 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 			                            .Create();
 
 
-			new ConfigurationContainer().Type<Section>().Member(p => p.IsSelected).Name("Selected")
-			                            .Type<Section>().Member(p => p.IsEmpty).Name("Empty")
+			new ConfigurationContainer().Type<Section>()
+				.Name("person")
+				.Member(p => p.IsSelected).Name("Selected").Identity().Order(2)
+														.Member(p => p.IsEmpty).Name("Empty")
+
 			                            .Create();
+
+
+			var serializer = new ConfigurationContainer()
+
+				.Type<Person>() // Configuration of Person class
+				.Member(p => p.Password) // First member
+				.Name("P")
+				.Encrypt()
+				.Member(p => p.Name) // Second member
+				.Name("T")
+				.UseEncryptionAlgorithm(new CustomEncryption())
+				.Type<TestClass>() // Configuration of another class
+				.CustomSerializer(new TestClassSerializer())
+				.Create();
+
+
+			new ConfigurationContainer()
+				.Type<Person>()
+				.Member(p => p.Name).Identity()
+				.OnlyConfiguredProperties()
+				.Create();
+
+			new ConfigurationContainer().Type<Person>()
+				.AddMigration(element => {})
+				.OnlyConfiguredProperties()
+				.Create();
+
+			new ConfigurationContainer().Type<Person>()
+				.OnlyConfiguredProperties()
+				.AddMigration(element => { })
+				.Create();
+
+			new ConfigurationContainer()
+				.Type<Person>()
+				.Member(p => p.Name).Identity()
+				.OnlyConfiguredProperties()
+				.Create();
+
+			new ConfigurationContainer()
+				.Type<Person>()
+				.OnlyConfiguredProperties()
+				.Member(p => p.Name).Identity()
+				.Create();
+
+			new ConfigurationContainer().Type<Person>()
+				.CustomSerializer((writer, person) => {}, element => new Person())
+				.EnableReferences(p=>p.Name);
+
+			new ConfigurationContainer().Type<Person>()
+				.EnableReferences(p => p.Name)
+				.CustomSerializer((writer, person) => { }, element => new Person());
+
+			new ConfigurationContainer().Type<Person>()
+				.CustomSerializer((writer, person) => { }, element => new Person())
+				.Member(p => p.Name).Identity();
+
+			new ConfigurationContainer().Type<Person>()
+				.Member(p => p.Name).Identity()
+				.CustomSerializer((writer, person) => { }, element => new Person());
+
+			new ConfigurationContainer().Type<Person>()
+				.AddMigration(element => { })
+				.Member(p => p.Name).Identity();
+
+			new ConfigurationContainer().Type<Person>()
+				.Member(p => p.Name)
+				.Identity()
+				.AddMigration(element => {});
 
 			var config = new ConfigurationContainer();
 			config.EnableDeferredReferences();
 			config.Type<Section>().Member(p => p.Id).Name("Identity");
 			config.Type<Section>().EnableReferences(p => p.Id);
 			var exs = config.Create();
+
+		}
+
+		public class TestClassSerializer : IExtendedXmlCustomSerializer<TestClass>
+		{
+			public TestClass Deserialize(XElement xElement)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public void Serializer(XmlWriter xmlWriter, TestClass obj)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+
+		public class Person
+		{
+			public string Name { get; set; }
+			public string Password { get; set; }
+		}
+
+		class CustomEncryption : IEncryption
+		{
+			public string Parse(string data)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public string Format(string instance)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+
+		public class TestClass
+		{
 
 		}
 
