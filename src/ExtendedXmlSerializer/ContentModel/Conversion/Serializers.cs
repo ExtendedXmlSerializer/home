495ff0b98c4df8569f808c251deb76b0a2b4effb
Mike-EEE <michael@dragonspark.us>
2018-01-25 17:09:05 -0500

    Addressing #157.

--- src/ExtendedXmlSerializer/ContentModel/Conversion/Serializers.cs
+++ src/ExtendedXmlSerializer/ContentModel/Conversion/Serializers.cs
@@ -32,18 +32,20 @@ namespace ExtendedXmlSerializer.ContentModel.Conversion
 		readonly IConverters _converters;
 		readonly IContentReaders _readers;
 		readonly IContentWriters _writers;
+		readonly Content.ISerializers _fallback;
 
-		public Serializers(IConverters converters, IContentReaders readers, IContentWriters writers)
+		public Serializers(IConverters converters, IContentReaders readers, IContentWriters writers, Content.ISerializers fallback)
 		{
 			_converters = converters;
 			_readers = readers;
 			_writers = writers;
+			_fallback = fallback;
 		}
 
 		protected override ISerializer Create(TypeInfo parameter)
 		{
 			var converter = _converters.Get(parameter);
-			var result = new Serializer(_readers.Get(converter.Parse), _writers.Get(converter.Format));
+			var result = converter != null ? new Serializer(_readers.Get(converter.Parse), _writers.Get(converter.Format)) : _fallback.Get(parameter);
 			return result;
 		}
 	}
---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue157Tests.cs
@@ -0,0 +1,39 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue157Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var serializer = new ConfigurationContainer().EnableParameterizedContent()
+			                                             .Create()
+			                                             .ForTesting();
+
+			serializer.Cycle(new Subject(null))
+			          .Another.Should()
+			          .BeNull();
+		}
+
+		struct Subject
+		{
+			public Subject(AnotherStruct? another) => Another = another;
+
+			public AnotherStruct? Another { get; }
+		}
+
+		struct AnotherStruct
+		{
+			[UsedImplicitly]
+			public AnotherStruct(int number) => Number = number;
+
+			public int Number { [UsedImplicitly] get; }
+		}
+	}
+}
