c2ff628ab9015b41351dd1c56d8b59fc47827cc4
wojtpl2 <wojtpl2@gmail.com>
2018-02-05 11:37:42 +0100

    #165 Add props files and mark JetBrains.Annotations refenrence as private assert and improve build.sh1

--- build.ps1
+++ build.ps1
@@ -53,14 +53,10 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 EnsurePsbuildInstalled
 
 exec { & dotnet restore .\src\ExtendedXmlSerializer }
-exec { & dotnet restore .\src\ExtendedXmlSerializer.Legacy }
 exec { & dotnet restore .\test\ExtendedXmlSerializer.Tests }
-exec { & dotnet restore .\test\ExtendedXmlSerializer.Tests.Legacy }
 
 exec { & dotnet build .\src\ExtendedXmlSerializer\ExtendedXmlSerializer.csproj -c Release }
-exec { & dotnet build .\src\ExtendedXmlSerializer.Legacy\ExtendedXmlSerializer.Legacy.csproj -c Release }
 exec { & dotnet build .\test\ExtendedXmlSerializer.Tests\ExtendedXmlSerializer.Tests.csproj -c Release }
-exec { & dotnet build .\test\ExtendedXmlSerializer.Tests.Legacy\ExtendedXmlSerializer.Tests.Legacy.csproj -c Release }
 
 #Set-MsBuild "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
 #Invoke-MSBuild .\src\ExtendedXmlSerializer\ExtendedXmlSerializer.csproj -configuration Release 
@@ -72,10 +68,8 @@ $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BU
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet test .\test\ExtendedXmlSerializer.Tests\ExtendedXmlSerializer.Tests.csproj -c Release }
-exec { & dotnet test .\test\ExtendedXmlSerializer.Tests.Legacy\ExtendedXmlSerializer.Tests.Legacy.csproj -c Release }
 
 exec { & dotnet pack .\src\ExtendedXmlSerializer\ExtendedXmlSerializer.csproj -c Release -o .\..\..\artifacts --version-suffix=$revision }  
-exec { & dotnet pack .\src\ExtendedXmlSerializer.Legacy\ExtendedXmlSerializer.Legacy.csproj -c Release -o .\..\..\artifacts --version-suffix=$revision }  
 # exec { & dotnet pack .\src\ExtendedXmlSerializer.Autofac -c Release -o .\artifacts --version-suffix=$revision }  
 # exec { & dotnet pack .\src\ExtendedXmlSerializer.AspCore -c Release -o .\artifacts --version-suffix=$revision}
 # exec { & dotnet pack .\src\ExtendedXmlSerializer.WebApi -c Release -o .\artifacts --version-suffix=$revision}
