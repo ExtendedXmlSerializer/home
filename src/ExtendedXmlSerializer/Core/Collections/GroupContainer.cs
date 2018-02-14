0ded594028118f71c3af03ddde4eb0477c7fa280
Mike-EEE <michael@dragonspark.us>
2018-02-14 18:14:08 -0500

    File-per-class.

--- src/ExtendedXmlSerializer/Core/Collections/GroupContainer.cs
+++ src/ExtendedXmlSerializer/Core/Collections/GroupContainer.cs
@@ -1,18 +1,18 @@
 // MIT License
-//
+// 
 // Copyright (c) 2016-2018 Wojciech Nagórski
 //                    Michael DeMond
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,9 +21,29 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using ExtendedXmlSerializer.ReflectionModel;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ExtendedXmlSerializer.Core.Sources;
 
 namespace ExtendedXmlSerializer.Core.Collections
 {
-	interface ITypedSortOrder : ITypedTable<int> {}
+	public class GroupContainer<T> : DelegatedSource<GroupName, ICollection<T>>, IGroupContainer<T>
+	{
+		readonly IOrderedDictionary<GroupName, ICollection<T>> _store;
+
+		public GroupContainer(IEnumerable<IGroup<T>> groups)
+			: this(groups, GroupPairs<T>.Default) {}
+
+		public GroupContainer(IEnumerable<IGroup<T>> groups, IGroupPairing<T> pairing)
+			: this(new OrderedDictionary<GroupName, ICollection<T>>(groups.Select(pairing.Get))) {}
+
+		public GroupContainer(IOrderedDictionary<GroupName, ICollection<T>> store) : base(store.GetValue) => _store = store;
+
+
+		public IEnumerator<T> GetEnumerator() => _store.SelectMany(x => x.Value)
+		                                               .GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
 }
--- src/ExtendedXmlSerializer/Core/Collections/Groups.cs
+++ src/ExtendedXmlSerializer/Core/Collections/Groups.cs
@@ -21,9 +21,32 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using ExtendedXmlSerializer.ReflectionModel;
+using ExtendedXmlSerializer.Core.Sources;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace ExtendedXmlSerializer.Core.Collections
 {
-	interface ITypedSortOrder : ITypedTable<int> {}
+	// ReSharper disable once PossibleInfiniteInheritance
+	class Groups<T> : ItemsBase<IGroup<T>>
+	{
+		readonly ImmutableArray<GroupName> _phases;
+		readonly Func<GroupName, IGroup<T>> _factory;
+
+		public Groups(IEnumerable<GroupName> phases) : this(phases, x => new Group<T>(x)) {}
+
+		public Groups(IEnumerable<GroupName> phases, Func<GroupName, IGroup<T>> factory)
+			: this(phases.ToImmutableArray(), factory) {}
+
+		public Groups(ImmutableArray<GroupName> phases, Func<GroupName, IGroup<T>> factory)
+		{
+			_phases = phases;
+			_factory = factory;
+		}
+
+		public override IEnumerator<IGroup<T>> GetEnumerator() => _phases.Select(_factory)
+		                                                                 .GetEnumerator();
+	}
 }
