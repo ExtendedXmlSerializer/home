f86e707fe9020c9fcb8800e47c44e632a249de71
Mike-EEE <michael@dragonspark.us>
2018-02-23 10:38:33 -0500

    Example for #172.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue172Tests.cs
@@ -0,0 +1,42 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ExtendedXmlSerializer.Tests.Support;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public class Issue172Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			new ConfigurationContainer()
+				.EnableImplicitTyping(typeof(Course), typeof(Student), typeof(List<Student>))
+				.Ignore(typeof(List<>).GetProperty(nameof(List<object>.Capacity)))
+				.ForTesting()
+				.Assert(new Course
+				{
+					Title = "Hello World",
+					Students = new List<Student>
+					{
+						new Student { Name =  "Name"}
+					}
+				},
+				@"<?xml version=""1.0"" encoding=""utf-8""?><Issue172Tests-Course><Title>Hello World</Title><Students><Issue172Tests-Student><Name>Name</Name></Issue172Tests-Student></Students></Issue172Tests-Course>"
+				);
+		}
+
+		public class Course
+		{
+			public string Title { get; set; }
+			public List<Student> Students { get; set; }
+		}
+
+		public class Student
+		{
+			public string Name { get; set; }
+		}
+	}
+}
