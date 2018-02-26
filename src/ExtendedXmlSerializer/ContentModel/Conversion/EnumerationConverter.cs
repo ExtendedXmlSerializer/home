9850244254f25bfe83121f6a6d338369ef021b86
Mike-EEE <michael@dragonspark.us>
2018-02-26 08:04:52 -0500

    XmlEnumAttribute support for #174.

--- src/ExtendedXmlSerializer/ContentModel/Conversion/EnumerationConverter.cs
+++ src/ExtendedXmlSerializer/ContentModel/Conversion/EnumerationConverter.cs
@@ -1,18 +1,18 @@
 // MIT License
-// 
+//
 // Copyright (c) 2016-2018 Wojciech Nagórski
 //                    Michael DeMond
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,26 +21,51 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
+using ExtendedXmlSerializer.Core;
 using ExtendedXmlSerializer.Core.Sources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
 
 namespace ExtendedXmlSerializer.ContentModel.Conversion
 {
-	class EnumerationConverter : Converter<Enum>
+	sealed class EnumerationConverter : Converter<Enum>
 	{
-		public EnumerationConverter(Type enumerationType) : base(new Source(enumerationType).Get, x => x.ToString()) {}
+		public EnumerationConverter(Type enumerationType)
+			: this(enumerationType,
+			       enumerationType.GetTypeInfo()
+			                      .GetMembers()
+			                      .Where(x => x.MemberType == MemberTypes.Field)
+			                      .ToDictionary(x => x.Name, x => x.GetCustomAttribute<XmlEnumAttribute>()
+			                                                       ?.Name ?? x.Name)) {}
 
-		class Source : IParameterizedSource<string, Enum>
+		public EnumerationConverter(Type enumerationType, IDictionary<string, string> values)
+			: base(new Reader(enumerationType, new TableSource<string, string>(values.ToDictionary(x => x.Value, x => x.Key)).Get).Get,
+			       new Writer(new TableSource<string, string>(values).Get).Get) {}
+
+		sealed class Reader : IParameterizedSource<string, Enum>
 		{
 			readonly Type _enumerationType;
+			readonly Func<string, string> _values;
 
-			public Source(Type enumerationType)
+			public Reader(Type enumerationType, Func<string, string> values)
 			{
 				_enumerationType = enumerationType;
+				_values = values;
 			}
 
 			public Enum Get(string parameter)
-				=> parameter != null ? (Enum) Enum.Parse(_enumerationType, parameter) : default(Enum);
+				=> parameter != null ? (Enum) Enum.Parse(_enumerationType, _values(parameter)) : default(Enum);
+		}
+
+		sealed class Writer : IFormatter<Enum>
+		{
+			readonly Func<string, string> _source;
+			public Writer(Func<string, string> source) => _source = source;
+
+			public string Get(Enum parameter) => _source(parameter.ToString());
 		}
 	}
 }
---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue174Tests.cs
@@ -0,0 +1,43 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.Support;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public class Issue174Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var support = new ConfigurationContainer().ForTesting();
+			support.Assert(SessionTypeEnum.MyTrainingList, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue174Tests-SessionTypeEnum xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests"">My Training List</Issue174Tests-SessionTypeEnum>");
+
+			support.Assert(new Subject{Enum = SessionTypeEnum.ServiceList},
+			               @"<?xml version=""1.0"" encoding=""utf-8""?><Issue174Tests-Subject xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Enum>Service List</Enum></Issue174Tests-Subject>");
+
+			support.Assert(SessionTypeEnum.NotDecorated, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue174Tests-SessionTypeEnum xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests"">NotDecorated</Issue174Tests-SessionTypeEnum>");
+		}
+
+		sealed class Subject
+		{
+			public SessionTypeEnum Enum { get; set; }
+
+		}
+
+		public enum SessionTypeEnum
+		{
+			/// <remarks />
+			[XmlEnum("My Training List")] MyTrainingList,
+
+			/// <remarks />
+			[XmlEnum("Training List")] TrainingList,
+
+			/// <remarks />
+			[XmlEnum("Service List")]
+			ServiceList,
+
+			NotDecorated
+		}
+	}
+}
