938e601c681d680737ae47dbeaf845d1982f92e7
Mike-EEE <michael@dragonspark.us>
2018-05-11 13:53:25 -0400

    Example for #183.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue183Tests.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ExtendedXmlSerializer.Tests.Support;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue183Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var serializer = new ConfigurationContainer().EnableImplicitTypingFromNested<Issue183Tests>()
+			                                             .UseOptimizedNamespaces()
+			                                             .Create()
+			                                             .ForTesting();
+			var instance = new Model {PropertyWithInterface = new Implementation()};
+
+			serializer.Assert(instance,
+			                  @"<?xml version=""1.0"" encoding=""utf-8""?><Issue183Tests-Model xmlns:exs=""https://extendedxmlserializer.github.io/v2""><PropertyWithInterface exs:type=""Issue183Tests-Implementation"" /></Issue183Tests-Model>");
+		}
+
+		sealed class Model
+		{
+			public IInterface PropertyWithInterface { get; set; }
+		}
+
+		interface IInterface {}
+
+		sealed class Implementation : IInterface {}
+	}
+}
