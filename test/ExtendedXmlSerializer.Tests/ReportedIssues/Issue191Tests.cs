078753dae55e67c3ffefb36ec4cf2f3c2500e745
Mike-EEE <michael@dragonspark.us>
2018-08-03 12:43:24 -0400

    Example for #191.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue191Tests.cs
@@ -0,0 +1,33 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ExtendedXmlSerializer.Tests.Support;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue191Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var test = new Test {Attribute = "aaa", Element = "bbb"};
+			new ConfigurationContainer().InspectingType<Test>()
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(test,
+			                                    @"<?xml version=""1.0"" encoding=""utf-8""?><Test Attribute=""aaa"" xmlns=""http://test.org/schema/test""><Element>bbb</Element></Test>");
+		}
+
+		[XmlType(Namespace = "http://test.org/schema/test", TypeName = "Test")]
+		[XmlRoot(Namespace = "http://test.org/schema/test", IsNullable = false)]
+		public class Test
+		{
+			[XmlAttribute]
+			public string Attribute { get; set; }
+
+			[XmlElement]
+			public string Element { get; set; }
+		}
+	}
+}
