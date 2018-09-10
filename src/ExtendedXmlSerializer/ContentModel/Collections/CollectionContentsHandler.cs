2e39a262b012a448484046552e72658e0ac9bde3
Mike-EEE <michael@dragonspark.us>
2018-09-10 14:04:22 -0400

    Fixing #201.

--- src/ExtendedXmlSerializer/ContentModel/Collections/CollectionContentsHandler.cs
+++ src/ExtendedXmlSerializer/ContentModel/Collections/CollectionContentsHandler.cs
@@ -21,6 +21,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using ExtendedXmlSerializer.ContentModel.Content;
+using ExtendedXmlSerializer.ContentModel.Identification;
+
 namespace ExtendedXmlSerializer.ContentModel.Collections
 {
 	sealed class CollectionContentsHandler : ICollectionContentsHandler
@@ -30,6 +33,10 @@ namespace ExtendedXmlSerializer.ContentModel.Collections
 		public CollectionContentsHandler(ICollectionAssignment assignment) => _assignment = assignment;
 
 		public void Handle(IListInnerContent contents, IReader reader)
-			=> _assignment.Assign(contents, reader.Get(contents.Get()));
+		{
+			var parameter = contents.Get();
+			var item = IdentityComparer.Default.Equals(parameter, NullElementIdentity.Default) ? null : reader.Get(parameter);
+			_assignment.Assign(contents, item);
+		}
 	}
 }
---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue201Tests.cs
@@ -0,0 +1,30 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue201Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var subject = new TestClass();
+			subject.TestProperty.Add(456);
+			var testClass = new ConfigurationContainer().Create()
+			                                            .ForTesting()
+			                                            .Cycle(subject);
+			testClass.ShouldBeEquivalentTo(subject);
+			testClass.TestProperty.Count.Should()
+			         .Be(5);
+
+		}
+	}
+
+	public class TestClass
+	{
+		public List<object> TestProperty { get; set; } = new List<object> { "Hello", null, "World!", 123 };
+	}
+}
