e3c45917b49cd595012c6da80dae62441e3e4cf0
Mike-EEE <michael@dragonspark.us>
2018-09-13 13:02:54 -0400

    Example code for #203.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue203Tests.cs
@@ -0,0 +1,33 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.Tests.Support;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue203Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new Person("I AM MY FATHER!");
+			instance.Father = instance;
+			new ConfigurationContainer().EnableReferences()
+			                            .EnableParameterizedContent()
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(instance, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue203Tests-Person xmlns:exs=""https://extendedxmlserializer.github.io/v2"" exs:identity=""1"" xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Name>I AM MY FATHER!</Name><Father exs:reference=""1"" /></Issue203Tests-Person>");
+		}
+
+		public class Person
+		{
+			public Person(string name) => Name = name;
+
+			public string Name { get; }
+
+			public Person Mother { get; set; }
+
+			public Person Father { get; set; }
+		}
+	}
+}
