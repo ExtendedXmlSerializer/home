6af21181441cd7921340c8227f4efea42611ffd1
Mike-EEE <michael@dragonspark.us>
2018-09-19 07:29:24 -0400

    Sample code for #206

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue206Tests.cs
@@ -0,0 +1,28 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue206Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var travelFiles = new[] {new TravelFile()};
+
+			var support = new ConfigurationContainer().EnableImplicitTyping(typeof(TravelFile[]), typeof(TravelFile))
+			                                          .Type<TravelFile[]>()
+			                                          .Name("ArrayOfTravelFile")
+			                                          .Create()
+			                                          .ForTesting();
+
+			support.Deserialize<TravelFile[]>(@"<?xml version=""1.0"" encoding=""utf-8""?><ArrayOfTravelFile><Issue206Tests-TravelFile /></ArrayOfTravelFile>")
+			       .ShouldBeEquivalentTo(travelFiles);
+		}
+
+		sealed class TravelFile {}
+	}
+}
