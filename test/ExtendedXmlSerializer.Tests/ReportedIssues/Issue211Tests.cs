a22caeee46a6876120ece21aad51f6e9e821b5ed
Mike-EEE <michael@dragonspark.us>
2018-10-08 07:10:06 -0400

    Example code for #211.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue211Tests.cs
@@ -0,0 +1,39 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue211Tests
+	{
+		[Fact]
+		void VerifyDefaultBehavior()
+		{
+			var instance = new Subject {Message = null};
+			new ConfigurationContainer().Create()
+			                            .Cycle(instance).Message
+			                            .Should()
+			                            .Be(Subject.DefaultValue);
+		}
+
+		[Fact]
+		void VerifyConfiguration()
+		{
+			var instance = new Subject {Message = null};
+			new ConfigurationContainer().Emit(EmitBehaviors.Assigned)
+			                            .Create()
+			                            .Cycle(instance)
+			                            .Message
+			                            .Should()
+			                            .BeNull();
+		}
+
+		sealed class Subject
+		{
+			public const string DefaultValue = "Default Value";
+			public string Message { get; set; } = DefaultValue;
+		}
+	}
+}
