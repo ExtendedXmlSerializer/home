3a5e76a748a0355b18658a1938bb3ec6928d04e2
Mike-EEE <michael@dragonspark.us>
2018-10-12 07:00:02 -0400

    Accounting for null values with variable-typed body writer #213.

--- src/ExtendedXmlSerializer/ContentModel/Members/VariableTypedMemberWriter.cs
+++ src/ExtendedXmlSerializer/ContentModel/Members/VariableTypedMemberWriter.cs
@@ -1,18 +1,18 @@
 // MIT License
-// 
+//
 // Copyright (c) 2016-2018 Wojciech Nagórski
 //                    Michael DeMond
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -51,14 +51,21 @@ namespace ExtendedXmlSerializer.ContentModel.Members
 		public void Write(IFormatWriter writer, object instance)
 		{
 			var type = instance?.GetType();
-			if (type != null && _type.IsSatisfiedBy(type))
+			if (type != null)
 			{
-				_property.Write(writer, type.GetTypeInfo());
-				_runtime.Write(writer, instance);
+				if (_type.IsSatisfiedBy(type))
+				{
+					_property.Write(writer, type.GetTypeInfo());
+					_runtime.Write(writer, instance);
+				}
+				else
+				{
+					_body.Write(writer, instance);
+				}
 			}
 			else
 			{
-				_body.Write(writer, instance);
+				writer.Content(null);
 			}
 		}
 	}
