e88fd3e9b68865f3dd8eda0d1c678312070f432d
Mike-EEE <michael@dragonspark.us>
2018-10-12 07:46:40 -0400

    Ensuring header.

--- src/ExtendedXmlSerializer/ExtensionModel/Content/Members/MemberExceptionHandlingExtension.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Content/Members/MemberExceptionHandlingExtension.cs
@@ -1,4 +1,27 @@
-﻿using ExtendedXmlSerializer.ContentModel.Content;
+﻿// MIT License
+//
+// Copyright (c) 2016-2018 Wojciech Nagórski
+//                    Michael DeMond
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using ExtendedXmlSerializer.ContentModel.Content;
 using ExtendedXmlSerializer.ContentModel.Format;
 using ExtendedXmlSerializer.ContentModel.Members;
 using ExtendedXmlSerializer.Core;
@@ -14,7 +37,8 @@ namespace ExtendedXmlSerializer.ExtensionModel.Content.Members
 		MemberExceptionHandlingExtension() {}
 
 		public IServiceRepository Get(IServiceRepository parameter)
-			=> parameter.Decorate<IMemberSerializers, MemberSerializers>().Decorate<IMemberHandler, MemberHandler>();
+			=> parameter.Decorate<IMemberSerializers, MemberSerializers>()
+			            .Decorate<IMemberHandler, MemberHandler>();
 
 		public void Execute(IServices parameter) {}
 
@@ -32,7 +56,9 @@ namespace ExtendedXmlSerializer.ExtensionModel.Content.Members
 				}
 				catch (Exception e)
 				{
-					var line = contents.Get().Get().To<IXmlLineInfo>();
+					var line = contents.Get()
+					                   .Get()
+					                   .To<IXmlLineInfo>();
 					throw new
 						InvalidOperationException($"An exception was encountered while deserializing member '{member.Profile.Metadata.ReflectedType}.{member.Profile.Name}'.",
 						                          new XmlException(e.Message, e, line.LineNumber, line.LinePosition));
