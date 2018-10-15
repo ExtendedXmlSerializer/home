b3a95cfb06c901c91a8b4c4bf799b707df5e02d9
Mike-EEE <michael@dragonspark.us>
2018-10-15 11:24:31 -0400

    Additional tests for #216.

--- test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue216Tests.cs
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue216Tests.cs
@@ -1,4 +1,5 @@
 ﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
 using ExtendedXmlSerializer.Tests.Support;
 using FluentAssertions;
 using System.Collections.Generic;
@@ -12,9 +13,78 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 		void Verify()
 		{
 			var instance = new Foo {Baz = {"hello"}};
-			var cycle = new ConfigurationContainer().Create()
+			var cycle = new ConfigurationContainer().Emit(EmitBehaviors.Assigned)
+			                                        .Create()
 			                                        .Cycle(instance);
 			cycle.ShouldBeEquivalentTo(instance);
+		}
+
+		[Fact]
+		void VerifyAlways()
+		{
+			var instance = new Foo {Baz = {"hello"}};
+			var cycle = new ConfigurationContainer().Emit(EmitBehaviors.Always)
+			                                        .Create()
+			                                        .Cycle(instance);
+			cycle.ShouldBeEquivalentTo(instance);
+		}
+
+		[Fact]
+		void Emit()
+		{
+			var instance = new Foo {Baz = {"hello"}};
+			new ConfigurationContainer().Emit(EmitBehaviors.Assigned)
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(instance, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue216Tests-Foo xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Baz><Capacity>4</Capacity><string xmlns=""https://extendedxmlserializer.github.io/system"">hello</string></Baz></Issue216Tests-Foo>");
+		}
+
+		[Fact]
+		void EmitAlways()
+		{
+			var instance = new Foo {Baz = {"hello"}};
+			new ConfigurationContainer().Emit(EmitBehaviors.Always)
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(instance, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue216Tests-Foo xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Baz><Capacity>4</Capacity><string xmlns=""https://extendedxmlserializer.github.io/system"">hello</string></Baz></Issue216Tests-Foo>");
+		}
+
+
+		[Fact]
+		void VerifyReadOnlyDoesNotEmit()
+		{
+			new ConfigurationContainer().Emit(EmitBehaviors.Always)
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(new Subject {Message = "Hello World!"},
+			                                    @"<?xml version=""1.0"" encoding=""utf-8""?><Issue216Tests-Subject xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Message>Hello World!</Message></Issue216Tests-Subject>");
+		}
+
+		[Fact]
+		void VerifyReadOnlyDoesNotEmitAssigned()
+		{
+			new ConfigurationContainer().Emit(EmitBehaviors.Assigned)
+			                            .Create()
+			                            .ForTesting()
+			                            .Assert(new Subject {Message = "Hello World!"},
+			                                    @"<?xml version=""1.0"" encoding=""utf-8""?><Issue216Tests-Subject xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests""><Message>Hello World!</Message></Issue216Tests-Subject>");
+		}
+
+		sealed class Subject
+		{
+			string _message;
+
+			public string Message
+			{
+				get => _message;
+				set
+				{
+					_message  = value;
+					Formatted = $"Yo: {_message}";
+				}
+			}
+
+			public string Formatted { get; private set; }
 		}
 
 		public class Foo
