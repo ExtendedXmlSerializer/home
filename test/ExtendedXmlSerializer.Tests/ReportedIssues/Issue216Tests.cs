ba89f35c591846c5ad7037593ab91e52123c6a94
Mike-EEE <michael@dragonspark.us>
2018-10-15 06:49:04 -0400

    Adding sample code for #216

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue216Tests.cs
@@ -0,0 +1,25 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue216Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new Foo {Baz = {"hello"}};
+			var cycle = new ConfigurationContainer().Create()
+			                                        .Cycle(instance);
+			cycle.ShouldBeEquivalentTo(instance);
+		}
+
+		public class Foo
+		{
+			public List<string> Baz { get; } = new List<string>();
+		}
+	}
+}
