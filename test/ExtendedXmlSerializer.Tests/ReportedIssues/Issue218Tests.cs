1056de9fa1faf9a1c43e37b7375555a741babd05
Mike-EEE <michael@dragonspark.us>
2018-10-16 14:16:46 -0400

    Sample code for #218.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue218Tests.cs
@@ -0,0 +1,29 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue218Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new MyListImpl<string>("Hello World!");
+			new ConfigurationContainer().EnableParameterizedContent()
+			                            .Create()
+			                            .Cycle(instance)
+			                            .ShouldBeEquivalentTo(instance);
+		}
+
+		public class MyListImpl<T> : List<T>
+		{
+			public MyListImpl(object owner) => Owner = owner;
+
+			public object Owner { get; }
+		}
+	}
+}
