c6e8c4545997bc6fa5a3d4aaeb9fa162523d5f57
Mike-EEE <michael@dragonspark.network>
2018-12-16 08:19:12 -0500

    Adding a specification for activating types with singletons #227.

--- src/ExtendedXmlSerializer/ExtensionModel/Types/Activators.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Types/Activators.cs
@@ -1,18 +1,18 @@
 // MIT License
-// 
+//
 // Copyright (c) 2016-2018 Wojciech Nagórski
 //                    Michael DeMond
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using ExtendedXmlSerializer.Core;
 using ExtendedXmlSerializer.Core.Sources;
 using ExtendedXmlSerializer.ReflectionModel;
 using System;
@@ -30,29 +31,37 @@ namespace ExtendedXmlSerializer.ExtensionModel.Types
 	sealed class Activators : ReferenceCacheBase<Type, IActivator>, IActivators
 	{
 		public static Activators Default { get; } = new Activators();
-		Activators() : this(DefaultActivators.Default, SingletonLocator.Default) {}
 
-		readonly IActivators _activators;
-		readonly ISingletonLocator _locator;
+		Activators() : this(ActivatingTypeSpecification.Default, DefaultActivators.Default, SingletonLocator.Default) {}
 
-		public Activators(IActivators activators, ISingletonLocator locator)
+		readonly IActivatingTypeSpecification _specification;
+		readonly IActivators                  _activators;
+		readonly ISingletonLocator            _locator;
+
+		public Activators(IActivatingTypeSpecification specification, IActivators activators, ISingletonLocator locator)
 		{
-			_activators = activators;
-			_locator = locator;
+			_specification = specification;
+			_activators    = activators;
+			_locator       = locator;
 		}
 
 		protected override IActivator Create(Type parameter)
 		{
 			var singleton = _locator.Get(parameter);
 			var activator = _activators.Build(parameter);
-			var result = singleton != null ? new Activator(_activators.Build(parameter), singleton) : activator();
+			var activate  = _specification.IsSatisfiedBy(parameter);
+			var result = singleton != null
+				             ? activate
+					               ? new Activator(_activators.Build(parameter), singleton)
+					               : (IActivator)new ReflectionModel.Activator(singleton.Self)
+				             : activator();
 			return result;
 		}
 
 		sealed class Activator : IActivator
 		{
 			readonly Func<IActivator> _activator;
-			readonly object _singleton;
+			readonly object           _singleton;
 
 			public Activator(Func<IActivator> activator, object singleton)
 			{
@@ -67,7 +76,8 @@ namespace ExtendedXmlSerializer.ExtensionModel.Types
 					return _activator()
 						.Get();
 				}
-				catch (Exception)
+				// ReSharper disable once CatchAllClause - In this case we fallback to singleton since it exists.
+				catch
 				{
 					return _singleton;
 				}
---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue227Tests.cs
@@ -0,0 +1,26 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue227Tests
+	{
+		readonly IExtendedXmlSerializer _serializer = new ConfigurationContainer().EnableReferences()
+		                                                                          .Create();
+
+		class FooBar
+		{
+			public string Foo { get; set; } = "Bar";
+		}
+
+		[Fact]
+		void Verify()
+		{
+			_serializer.Serialize(new FooBar())
+			           .Should()
+			           .NotBeNull();
+		}
+	}
+}
