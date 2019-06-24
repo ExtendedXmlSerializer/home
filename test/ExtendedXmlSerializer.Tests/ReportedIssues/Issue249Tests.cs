d82db1f9515eda61ba0ef27887d4ced30265e713
Mike-EEE <michael@dragonspark.network>
2019-06-24 11:44:20 -0400

    Fix for #249.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue249Tests.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Content;
+using ExtendedXmlSerializer.ExtensionModel.Types;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Drawing;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue249Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new DataHolder();
+
+			new ConfigurationContainer().EnableAllConstructors()
+			                            .EnableParameterizedContent()
+			                            .EnableReferences()
+			                            .Create()
+			                            .ForTesting()
+			                            .Cycle(instance)
+			                            .ShouldBeEquivalentTo(instance);
+		}
+
+		sealed class DataHolder
+		{
+			public Size Size { get; set; } = new Size(100, 200);
+		}
+	}
+}
