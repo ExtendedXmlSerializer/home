2a820ece4eccc4810240229315141d7607291106
Mike-EEE <michael@dragonspark.network>
2019-07-04 11:28:36 -0400

    Sample code for #255.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue255Tests.cs
@@ -0,0 +1,42 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue255Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new BackingFieldExample().Initialized();
+
+			new ConfigurationContainer().Create()
+			                            .ForTesting()
+			                            .Cycle(instance)
+			                            .ShouldBeEquivalentTo(instance);
+		}
+
+		public class BackingFieldExample
+		{
+			[XmlElement]
+			readonly IList<string> _values = new List<string>();
+
+			public BackingFieldExample Initialized()
+			{
+				AddValue("Hello");
+				AddValue("World");
+				return this;
+			}
+
+			public IReadOnlyCollection<string> Values => this._values.ToList().AsReadOnly();
+
+			public void AddValue(string value) => this._values.Add(value);
+		}
+	}
+}
