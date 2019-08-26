c10aad560ffd276bcfa5cdee83a8753885f80c58
Mike-EEE <michael@dragonspark.network>
2019-08-26 13:49:57 -0400

    Sample code for #263.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue263Tests.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue263Tests
+	{
+		[Fact]
+		public void ShouldDeserializeXMLtoObject()
+		{
+			const string xmlstring =
+				@"<?xml version=""1.0"" encoding=""utf-8""?><Issue263Tests-FooBar><Foo>Test 1</Foo><Bar>Test 2</Bar></Issue263Tests-FooBar>";
+			var foobar = new FooBar {Foo = "Test 1", Bar = "Test 2"};
+			var serializer = new ConfigurationContainer().EnableImplicitTypingFromNested<Issue263Tests>()
+			                                             .Create();
+			var result = serializer.Deserialize<FooBar>(xmlstring);
+			result.Foo.Should()
+			      .Be(foobar.Foo, "Deserialization failed with prop Foo");
+
+			result.Bar.Should()
+			      .Be(foobar.Bar, "Deserialization failed with prop Bar");
+		}
+
+		public class FooBar
+		{
+			public string Foo { get; set; }
+			public string Bar { get; set; }
+		}
+	}
+}
