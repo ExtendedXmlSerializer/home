19949b2270b2a0626f46e9816798f492706e5103
Mike-EEE <michael@dragonspark.network>
2019-08-29 07:53:34 -0400

    Removing auto-generated classes from implicit types. #263

--- src/ExtendedXmlSerializer/ExtensionModel/Xml/ImplicitTypingExtension.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Xml/ImplicitTypingExtension.cs
@@ -25,6 +25,7 @@ using ExtendedXmlSerializer.ContentModel.Identification;
 using ExtendedXmlSerializer.ContentModel.Reflection;
 using ExtendedXmlSerializer.Core;
 using ExtendedXmlSerializer.Core.Sources;
+using ExtendedXmlSerializer.ReflectionModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -39,7 +40,6 @@ namespace ExtendedXmlSerializer.ExtensionModel.Xml
 
 		public ImplicitTypingExtension(ImmutableArray<TypeInfo> registered) => _registered = registered;
 
-
 		public IServiceRepository Get(IServiceRepository parameter)
 			=> parameter.Decorate<IIdentities>(Register)
 			            .Decorate<ITypeIdentities>(Register);
@@ -47,8 +47,8 @@ namespace ExtendedXmlSerializer.ExtensionModel.Xml
 		ITypeIdentities Register(IServiceProvider services, ITypeIdentities identities)
 		{
 			var formatter = services.Get<ITypeFormatter>();
-			var entries = new Types(formatter).Get(_registered);
-			var result = new ImplicitTypeIdentities(identities, entries);
+			var entries   = new Types(formatter).Get(_registered);
+			var result    = new ImplicitTypeIdentities(identities, entries);
 			return result;
 		}
 
@@ -59,27 +59,40 @@ namespace ExtendedXmlSerializer.ExtensionModel.Xml
 
 		sealed class Types : IParameterizedSource<ImmutableArray<TypeInfo>, IParameterizedSource<string, TypeInfo>>
 		{
-			readonly ITypeFormatter _formatter;
+			readonly ITypeFormatter       _formatter;
+			readonly Func<TypeInfo, bool> _specification;
 
-			public Types(ITypeFormatter formatter) => _formatter = formatter;
+			public Types(ITypeFormatter formatter)
+				: this(formatter, ApplicationTypeSpecification.Default.IsSatisfiedBy) {}
+
+			public Types(ITypeFormatter formatter, Func<TypeInfo, bool> specification)
+			{
+				_formatter     = formatter;
+				_specification = specification;
+			}
 
 			public IParameterizedSource<string, TypeInfo> Get(ImmutableArray<TypeInfo> parameter)
 			{
-				var items = parameter.Select(Item).ToArray();
-				var groups = items.GroupBy(x => x.Key).ToArray();
+				var items = parameter.Where(_specification)
+				                     .Select(Item)
+				                     .ToArray();
+				var groups = items.GroupBy(x => x.Key)
+				                  .ToArray();
 				var invalid = groups.FirstOrDefault(x => x.Count() > 1);
 				if (invalid != null)
 				{
 					var types = string.Join(", ", invalid.Select(x => x.Value.FullName));
 					throw new InvalidOperationException(
-						$"An attempt was made to configure implicit types, but there is more than one type with the same name, which would result in conflicts.  Please remove one of these types or configure them to have different names from each other: {types} have the shared name {invalid.Key}.");
+					                                    $"An attempt was made to configure implicit types, but there is more than one type with the same name, which would result in conflicts.  Please remove one of these types or configure them to have different names from each other: {types} have the shared name {invalid.Key}.");
 				}
-				var store = items.ToDictionary();
+
+				var store  = items.ToDictionary();
 				var result = new TableSource<string, TypeInfo>(store);
 				return result;
 			}
 
-			KeyValuePair<string, TypeInfo> Item(TypeInfo parameter) => Pairs.Create(_formatter.Get(parameter), parameter);
+			KeyValuePair<string, TypeInfo> Item(TypeInfo parameter)
+				=> Pairs.Create(_formatter.Get(parameter), parameter);
 		}
 	}
 }
---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue262Tests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue262Tests
+	{
+
+	}
+}
