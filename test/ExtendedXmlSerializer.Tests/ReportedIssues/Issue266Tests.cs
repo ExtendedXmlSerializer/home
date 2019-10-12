8fd479a1045cc9191ff3480024913f1fd0d6fbdc
Mike-EEE <michael@dragonspark.network>
2019-10-12 07:08:29 -0400

    Sample code for #266.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue266Tests.cs
@@ -0,0 +1,29 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using System.IO;
+using System.Xml;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+    public sealed class Issue266Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var m = new XMLModMetadata {Author = "me", Name = "test", Description = "desc"};
+			var ms = new MemoryStream();
+			using (var xw = XmlWriter.Create(ms))
+			{
+				new ConfigurationContainer().Create()
+											.Serialize(xw, m);
+				xw.Flush();
+			}
+		}
+
+		class XMLModMetadata {
+			public string Author { get; set; }
+			public string Name { get; set; }
+			public string Description { get; set; }
+		}
+	}
+}
