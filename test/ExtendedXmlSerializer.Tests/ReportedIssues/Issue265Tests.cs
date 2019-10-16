d609197f9bb3d61de838cb43d826c00798bd2256
Mike-EEE <michael@dragonspark.network>
2019-10-16 01:49:57 -0400

    Sample code for #265.

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue265Tests.cs
@@ -0,0 +1,34 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.Support;
+using FluentAssertions;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue265Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			const string message = "Hello World!";
+			new ConfigurationContainer().Create()
+			                            .ForTesting()
+			                            .Cycle(new Subject(message))
+			                            .Message()
+			                            .Should()
+			                            .Be(message);
+		}
+
+		sealed class Subject
+		{
+			[XmlElement] string _message;
+
+			public Subject() : this("Default Value") {}
+
+			public Subject(string test) => _message = test;
+
+			public string Message() => _message;
+		}
+	}
+}
