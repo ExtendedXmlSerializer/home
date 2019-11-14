c5197a343f1c1d0344eb2ef8eff48f1a174a44f5
Mike-EEE <michael@dragonspark.network>
2019-11-14 07:55:57 -0500

    Documentation: content alteration extension methods. #189

--- src/ExtendedXmlSerializer/ExtensionMethods.Model.Alteration.cs
+++ src/ExtendedXmlSerializer/ExtensionMethods.Model.Alteration.cs
@@ -11,28 +11,100 @@ namespace ExtendedXmlSerializer
 	// ReSharper disable once MismatchedFileName
 	public static partial class ExtensionMethods
 	{
+		/// <summary>
+		/// Used to alter an instance of the configured result type whenever it is encountered during the serialization
+		/// process.  This can be used in scenarios where it is desired to know when an instance of a particular type is
+		/// emitted (for logging purposes, etc.) or, more generally, to alter it in some way (scrubbing data, etc)
+		/// before it is written to the external stream.  You can consider this as a value interception of
+		/// the serialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The type under configuration.</typeparam>
+		/// <param name="this">The type configuration to configure.</param>
+		/// <param name="write">The alteration delegate to invoke during writing.</param>
+		/// <returns>The configured type configuration.</returns>
 		public static ITypeConfiguration<T> Alter<T>(this ITypeConfiguration<T> @this, Func<T, T> write)
 			=> @this.Alter(Self<T>.Default.Get, write);
 
+		/// <summary>
+		/// Used to alter an instance of the configured result type whenever it is encountered during the serialization or
+		/// deserialization process.  This can be used in scenarios where it is desired to know when an instance of a
+		/// particular type is emitted or read (for logging purposes, etc.) or, more generally, to alter it in some way
+		/// (scrubbing data, etc) before it is written to the external stream or read into memory.  You can consider this as a
+		/// value interception of the serialization/deserialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The type under configuration.</typeparam>
+		/// <param name="this">The type configuration to configure.</param>
+		/// <param name="read">The alteration delegate to invoke during reading.</param>
+		/// <param name="write">The alteration delegate to invoke during writing.</param>
+		/// <returns>The configured type configuration.</returns>
 		public static ITypeConfiguration<T> Alter<T>(this ITypeConfiguration<T> @this, Func<T, T> read,
 		                                             Func<T, T> write)
 			=> @this.Alter(new DelegatedAlteration<T>(read), new DelegatedAlteration<T>(write));
 
+		/// <summary>
+		/// Used to alter an instance of the configured result type whenever it is encountered during the serialization or
+		/// deserialization process.  This can be used in scenarios where it is desired to know when an instance of a
+		/// particular type is emitted or read (for logging purposes, etc.) or, more generally, to alter it in some way
+		/// (scrubbing data, etc) before it is written to the external stream or read into memory.  You can consider this as a
+		/// value interception of the serialization/deserialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The type under configuration.</typeparam>
+		/// <param name="this">The type configuration to configure.</param>
+		/// <param name="read">The alteration to apply during reading.</param>
+		/// <param name="write">The alteration to apply during writing.</param>
+		/// <returns>The configured type configuration.</returns>
 		public static ITypeConfiguration<T> Alter<T>(this ITypeConfiguration<T> @this, IAlteration<T> read,
 		                                             IAlteration<T> write)
 			=> @this.Root.With<AlteredContentExtension>()
 			        .Types.Apply(Support<T>.Metadata, new ContentAlteration(read.Adapt(), write.Adapt()))
 			        .Return(@this);
 
+		/// <summary>
+		/// Used to alter the value of a member whenever it is encountered during the serialization process.  This can be used
+		/// in scenarios where it is desired to know when a the value of a member is emitted (for logging purposes,
+		/// etc.) or, more generally, to alter it in some way (scrubbing data, etc) before it is written to the external
+		/// stream.  You can consider this as a member value interception of the serialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The containing type under configuration.</typeparam>
+		/// <typeparam name="TMember">The member's value type.</typeparam>
+		/// <param name="this">The member configuration under configuration.</param>
+		/// <param name="write">The alteration delegate to invoke on the member value when it is written.</param>
+		/// <returns>The configured member configuration.</returns>
 		public static IMemberConfiguration<T, TMember> Alter<T, TMember>(this IMemberConfiguration<T, TMember> @this,
 		                                                                 Func<TMember, TMember> write)
-			=> @this.Alter(Self<TMember>.Default.Get, write);
+			=> @this.Alter(Self.DelegateOf<TMember>(), write);
 
+		/// <summary>
+		/// Used to alter the value of a member whenever it is encountered during the serialization or deserialization
+		/// process.  This can be used in scenarios where it is desired to know when a the value of a member is emitted or
+		/// read (for logging purposes, etc.) or, more generally, to alter it in some way (scrubbing data, etc) before it is
+		/// written to the external stream or read into memory.  You can consider this as a member value interception of the
+		/// serialization/deserialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The containing type under configuration.</typeparam>
+		/// <typeparam name="TMember">The member's value type.</typeparam>
+		/// <param name="this">The member configuration under configuration.</param>
+		/// <param name="read">The alteration delegate to invoke on the member value when it is read.</param>
+		/// <param name="write">The alteration delegate to invoke on the member value when it is written.</param>
+		/// <returns>The configured member configuration.</returns>
 		public static IMemberConfiguration<T, TMember> Alter<T, TMember>(this IMemberConfiguration<T, TMember> @this,
 		                                                                 Func<TMember, TMember> read,
 		                                                                 Func<TMember, TMember> write)
 			=> @this.Alter(new DelegatedAlteration<TMember>(read), new DelegatedAlteration<TMember>(write));
 
+		/// <summary>
+		/// Used to alter the value of a member whenever it is encountered during the serialization or deserialization
+		/// process.  This can be used in scenarios where it is desired to know when a the value of a member is emitted or
+		/// read (for logging purposes, etc.) or, more generally, to alter it in some way (scrubbing data, etc) before it is
+		/// written to the external stream or read into memory.  You can consider this as a member value interception of the
+		/// serialization/deserialization pipeline.
+		/// </summary>
+		/// <typeparam name="T">The containing type under configuration.</typeparam>
+		/// <typeparam name="TMember">The member's value type.</typeparam>
+		/// <param name="this">The member configuration under configuration.</param>
+		/// <param name="read">The alteration to apply to the member value when it is read.</param>
+		/// <param name="write">The alteration to apply to the member value when it is written.</param>
+		/// <returns>The configured member configuration.</returns>
 		public static IMemberConfiguration<T, TMember> Alter<T, TMember>(this IMemberConfiguration<T, TMember> @this,
 		                                                                 IAlteration<TMember> read,
 		                                                                 IAlteration<TMember> write)
