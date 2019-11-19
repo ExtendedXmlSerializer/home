5dada87507d1b510d790cddec06e821358893029
Mike-EEE <michael@dragonspark.network>
2019-11-19 11:08:09 -0500

    Removing RDP code.

--- .build/Install.ps1
+++ .build/Install.ps1
@@ -4,7 +4,5 @@ if($documentation)
     choco install docfx -y
 }
 
-# git submodule update --rebase --remote
 git submodule -q update --init
-
-$blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+git submodule update --rebase --remote
