91a2e944592269f59536ff95ceae2f9a6d5d60c9
Mike-EEE <michael@dragonspark.network>
2019-11-19 10:45:39 -0500

    Reordering submodule  call.

--- .build/Install.ps1
+++ .build/Install.ps1
@@ -1,8 +1,8 @@
-git submodule update --rebase --remote
-# git submodule -q update --init
-
 if($documentation)
 {
     git checkout $env:APPVEYOR_REPO_BRANCH -q
     choco install docfx -y
 }
+
+git submodule update --rebase --remote
+# git submodule -q update --init
