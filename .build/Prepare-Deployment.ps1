f83d085247b1bc10ed5afde9d533b440b7ae0293
Mike-EEE <michael@dragonspark.network>
2019-11-27 14:54:03 -0500

    Build fix.

--- .build/Prepare-Deployment.ps1
+++ .build/Prepare-Deployment.ps1
@@ -1,7 +1,7 @@
 choco install docfx -y
 Install-Module -Name PowerShellForGitHub
 
-$branch = @{ $true = "master"; $false = $env:APPVEYOR_REPO_BRANCH; }[$env:APPVEYOR_FORCED_BUILD]
+$branch = @{ $true = "master"; $false = $env:APPVEYOR_REPO_BRANCH; }[[bool]$env:APPVEYOR_FORCED_BUILD]
 git checkout $branch -q
 
 git submodule update --rebase --remote
