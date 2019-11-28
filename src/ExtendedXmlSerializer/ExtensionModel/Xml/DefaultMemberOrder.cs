11dc9eb34bc52fac0448fbd4b0bfd2c18f7d8593
Mike-EEE <michael@dragonspark.network>
2019-11-28 08:59:13 -0500

    Exception check for UWP-based scenarios. (*sigh*)

--- src/ExtendedXmlSerializer/ExtensionModel/Xml/DefaultMemberOrder.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Xml/DefaultMemberOrder.cs
@@ -1,4 +1,6 @@
 ﻿using ExtendedXmlSerializer.Core.Sources;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Xml.Serialization;
 
@@ -14,10 +16,28 @@ namespace ExtendedXmlSerializer.ExtensionModel.Xml
 		/// </summary>
 		public static DefaultMemberOrder Default { get; } = new DefaultMemberOrder();
 
-		DefaultMemberOrder() {}
+		DefaultMemberOrder() : this(Members.Default) {}
+
+
+		readonly IParameterizedSource<MemberInfo, List<MemberInfo>> _members;
+
+		/// <inheritdoc />
+		public DefaultMemberOrder(IParameterizedSource<MemberInfo, List<MemberInfo>> members) => _members = members;
 
 		/// <inheritdoc />
 		public int Get(MemberInfo parameter)
-			=> DefaultXmlElementAttribute.Default.Get(parameter)?.Order ?? parameter.MetadataToken;
+			=> DefaultXmlElementAttribute.Default.Get(parameter)?.Order ?? Resolve(parameter);
+
+		int Resolve(MemberInfo parameter)
+		{
+			try
+			{
+				return parameter.MetadataToken;
+			}
+			catch (InvalidOperationException) // UWP Throws, because UWP: https://github.com/ExtendedXmlSerializer/home/issues/269#issuecomment-559485714
+			{
+				return _members.Get(parameter).IndexOf(parameter);
+			}
+		}
 	}
 }
---
+++ src/ExtendedXmlSerializer/ExtensionModel/Xml/Members.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ExtendedXmlSerializer.Core.Sources;
+
+namespace ExtendedXmlSerializer.ExtensionModel.Xml
+{
+	sealed class Members : ReferenceCache<MemberInfo, List<MemberInfo>>
+	{
+		public static Members Default { get; } = new Members();
+
+		Members() : base(x => x.ReflectedType.GetMembers().ToList()) {}
+	}
+}
