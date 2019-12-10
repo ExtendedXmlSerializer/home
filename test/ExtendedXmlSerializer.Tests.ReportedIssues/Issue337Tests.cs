3886ae5d4458ace177c5de4d31d511f2e5c4dfb8
Mike-EEE <Mike-EEE@users.noreply.github.com>
2019-12-10 03:04:17 -0500

    Fixed bug with null arrays when EmitBehaviors.WhenModified is us… (#338)
    * Fixed bug with null arrays when EmitBehaviors.WhenModified is used.
    
    * Attended to warnings.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue337Tests.cs
@@ -0,0 +1,29 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue337Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new TestClass();
+			new ConfigurationContainer().Emit(EmitBehaviors.WhenModified)
+			                            .Create()
+			                            .ForTesting()
+			                            .Cycle(instance)
+			                            .Should()
+			                            .BeEquivalentTo(instance);
+		}
+
+		class TestClass
+		{
+			[UsedImplicitly]
+			public string[] Array { get; set; }
+		}
+	}
+}
