ff7aab0c4d3eb35899624d1d5b6023e2d2f85034
Mike-EEE <michael@dragonspark.network>
2019-12-11 01:35:42 -0500

    Added failing test.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue340Tests.cs
@@ -0,0 +1,44 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue340Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new Subject { Contents = new[] {"Hello", "World!"}.Select(x => x) };
+			new ConfigurationContainer().ForTesting()
+			                            .Cycle(instance)
+			                            .Should()
+			                            .BeEquivalentTo(instance);
+		}
+
+		[Fact]
+		void VerifyImmutable()
+		{
+			var instance = new ImmutableSubject(new[] {"Hello", "World!"});
+			new ConfigurationContainer().ForTesting()
+			                            .Cycle(instance)
+			                            .Should()
+			                            .BeEquivalentTo(instance);
+		}
+
+		sealed class Subject
+		{
+			public IEnumerable<string> Contents { get; set; }
+		}
+
+		sealed class ImmutableSubject
+		{
+			public ImmutableSubject(IEnumerable<string> contents) => Contents = contents;
+
+			public IEnumerable<string> Contents { get; }
+		}
+	}
+}
