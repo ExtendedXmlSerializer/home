445dc73778743a947401945e16ed2e40709302ee
Mike-EEE <michael@dragonspark.network>
2020-01-28 07:10:43 -0500

    Removing recursion check for types that have custom serializers defined for them.

--- src/ExtendedXmlSerializer/ExtensionModel/References/ReferenceAwareSerializers.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/References/ReferenceAwareSerializers.cs
@@ -1,6 +1,7 @@
 using ExtendedXmlSerializer.ContentModel.Content;
 using ExtendedXmlSerializer.ContentModel.Format;
 using ExtendedXmlSerializer.Core.Specifications;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -10,19 +11,24 @@ namespace ExtendedXmlSerializer.ExtensionModel.References
 	sealed class ReferenceAwareSerializers : ISerializers
 	{
 		readonly ISpecification<object>        _conditions;
+		readonly ISpecification<TypeInfo>      _contains;
 		readonly IStaticReferenceSpecification _specification;
 		readonly IReferences                   _references;
 		readonly ISerializers                  _serializers;
 
-		public ReferenceAwareSerializers(IStaticReferenceSpecification specification, IReferences references,
+		// ReSharper disable once TooManyDependencies
+		public ReferenceAwareSerializers(IStaticReferenceSpecification specification,
+		                                 IContainsCustomSerialization custom, IReferences references,
 		                                 ISerializers serializers)
-			: this(new InstanceConditionalSpecification(), specification, references, serializers) {}
+			: this(new InstanceConditionalSpecification(), custom, specification, references, serializers) {}
 
 		// ReSharper disable once TooManyDependencies
-		public ReferenceAwareSerializers(ISpecification<object> conditions, IStaticReferenceSpecification specification,
-		                                 IReferences references, ISerializers serializers)
+		public ReferenceAwareSerializers(ISpecification<object> conditions, ISpecification<TypeInfo> contains,
+		                                 IStaticReferenceSpecification specification, IReferences references,
+		                                 ISerializers serializers)
 		{
 			_conditions    = conditions;
+			_contains      = contains;
 			_specification = specification;
 			_references    = references;
 			_serializers   = serializers;
@@ -32,20 +38,24 @@ namespace ExtendedXmlSerializer.ExtensionModel.References
 		{
 			var serializer = _serializers.Get(parameter);
 			var result = _specification.IsSatisfiedBy(parameter)
-				             ? new Serializer(_conditions, _references, serializer)
+				             ? new Serializer(AssignedSpecification<TypeInfo>.Default.And(_contains.Inverse()),
+				                              _conditions, _references, serializer)
 				             : serializer;
 			return result;
 		}
 
 		sealed class Serializer : ContentModel.ISerializer
 		{
+			readonly ISpecification<TypeInfo> _default;
 			readonly ISpecification<object>   _conditions;
 			readonly IReferences              _references;
 			readonly ContentModel.ISerializer _container;
 
-			public Serializer(ISpecification<object> conditions, IReferences references,
-			                  ContentModel.ISerializer container)
+			// ReSharper disable once TooManyDependencies
+			public Serializer(ISpecification<TypeInfo> @default, ISpecification<object> conditions,
+			                  IReferences references, ContentModel.ISerializer container)
 			{
+				_default    = @default;
 				_conditions = conditions;
 				_references = references;
 				_container  = container;
@@ -57,18 +67,21 @@ namespace ExtendedXmlSerializer.ExtensionModel.References
 			{
 				if (_conditions.IsSatisfiedBy(writer.Get()))
 				{
-					var references = _references.Get(instance);
-					if (references.Any())
+					var typeInfo = instance?.GetType()
+					                       .GetTypeInfo();
+					if (_default.IsSatisfiedBy(typeInfo))
 					{
-						var typeInfo = instance.GetType()
-						                       .GetTypeInfo();
-						var line = Environment.NewLine;
-						var message =
-							$"{line}{line}Here is a list of found references:{line}{string.Join(line, references.Select(x => $"- {x}"))}";
+						var references = _references.Get(instance);
+						if (references.Any())
+						{
+							var line = Environment.NewLine;
+							var message =
+								$"{line}{line}Here is a list of found references:{line}{string.Join(line, references.Select(x => $"- {x}"))}";
 
-						throw new CircularReferencesDetectedException(
-						                                              $"The provided instance of type '{typeInfo}' contains circular references within its graph. Serializing this instance would result in a recursive, endless loop. To properly serialize this instance, please create a serializer that has referential support enabled by extending it with the ReferencesExtension.{message}",
-						                                              _container);
+							throw new CircularReferencesDetectedException(
+							                                              $"The provided instance of type '{typeInfo}' contains circular references within its graph. Serializing this instance would result in a recursive, endless loop. To properly serialize this instance, please create a serializer that has referential support enabled by extending it with the ReferencesExtension.{message}",
+							                                              _container);
+						}
 					}
 				}
 
---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue361Tests.cs
@@ -0,0 +1,68 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue361Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var serializer = new ConfigurationContainer().EnableParameterizedContentWithPropertyAssignments()
+			                                             .Type<AdornedImage>()
+			                                             .CustomSerializer(new AdornedImageSerializer())
+			                                             .Create()
+			                                             .ForTesting();
+
+			serializer.Assert(new AdornedImage(),
+			                  @"<?xml version=""1.0"" encoding=""utf-8""?><Issue361Tests-AdornedImage xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests.ReportedIssues"" />");
+		}
+
+		class AdornedImageSerializer : IExtendedXmlCustomSerializer<AdornedImage>
+		{
+			public AdornedImage Deserialize(XElement xElement)
+			{
+				return new AdornedImage();
+			}
+
+			public void Serializer(XmlWriter xmlWriter, AdornedImage obj) {}
+		}
+
+		class AdornedImage
+		{
+			public vector Vector { get; set; }
+
+			public AdornedImage()
+			{
+				var data = new length(23);
+				Vector = new vector(data, data);
+			}
+		}
+
+		class length
+		{
+			public length(int value)
+			{
+				Value = value;
+			}
+
+			public int Value { get; }
+		}
+
+		private struct vector
+		{
+			public vector(length l1, length l2)
+			{
+				L1 = l1;
+				L2 = l2;
+			}
+
+			public length L1 { get; }
+			public length L2 { get; }
+		}
+	}
+}
