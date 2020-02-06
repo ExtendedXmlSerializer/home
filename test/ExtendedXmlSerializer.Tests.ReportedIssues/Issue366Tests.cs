597e51031b4484e8d57f49fe786e259329778904
Mike-EEE <michael@dragonspark.network>
2020-02-06 15:21:39 -0500

    Adjusted assembly-loading to be a bit more robust for .NETFramework-based GAC assemblies.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue366Tests.cs
@@ -0,0 +1,37 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Drawing;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue366Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = new Model();
+			new ConfigurationContainer().Create()
+			                            .ForTesting()
+			                            .Cycle(instance)
+			                            .Should()
+			                            .BeEquivalentTo(instance);
+		}
+
+		public class Model
+		{
+			public Model()
+			{
+				NestedModel = new NestedModel();
+			}
+
+			public NestedModel NestedModel { get; set; }
+		}
+
+		public class NestedModel
+		{
+			public Size Size { get; set; }
+		}
+	}
+}
