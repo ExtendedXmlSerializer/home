1948c4273776577be428a429d5f103100415e7fd
Mike-EEE <michael@dragonspark.network>
2020-03-09 11:06:42 -0400

    Sample code.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue372Tests.cs
@@ -0,0 +1,22 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue372Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var serializer = new ConfigurationContainer().UseOptimizedNamespaces()
+			                                             .EnableParameterizedContent()
+			                                             .Create()
+			                                             .ForTesting();
+			var instance = new KeyValuePair<string, object>("123", "123");
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+	}
+}
