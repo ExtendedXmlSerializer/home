bcd92354eecd74f55f2b2aea7ee7e79bf7894929
Mike-EEE <michael@dragonspark.network>
2020-03-17 11:13:30 -0400

    Added sample code.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue377Tests.cs
@@ -0,0 +1,58 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ContentModel.Format;
+using ExtendedXmlSerializer.ExtensionModel.Instances;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue377Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var serializer = new ConfigurationContainer().Type<Data1>()
+			                                             .WithMonitor(Monitor.Default)
+			                                             .Create()
+			                                             .ForTesting();
+			var instance = new Data1();
+			instance.MyCollection.Clear();
+			instance.MyCollection.Add("data4");
+			instance.MyCollection.Add("data5");
+			instance.MyCollection.Add("data6");
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		sealed class Monitor : ISerializationMonitor<Data1>
+		{
+			public static Monitor Default { get; } = new Monitor();
+
+			Monitor() {}
+
+			public void OnSerializing(IFormatWriter writer, Data1 instance) {}
+
+			public void OnSerialized(IFormatWriter writer, Data1 instance) {}
+
+			public void OnDeserializing(IFormatReader reader, Type instanceType) {}
+
+			public void OnActivating(IFormatReader reader, Type instanceType) {}
+
+			public void OnActivated(Data1 instance)
+			{
+				instance.MyCollection.Clear();
+			}
+
+			public void OnDeserialized(IFormatReader reader, Data1 instance) {}
+		}
+
+		sealed class Data1
+		{
+			public Data1() => MyCollection = new List<string> {"data1", "data2", "data3"};
+
+			public List<string> MyCollection { get; }
+		}
+	}
+}
