ecb712eb1c7ccdbffbf08be8f436610b4db02bac
Mike-EEE <Mike-EEE@users.noreply.github.com>
2020-03-19 10:25:01 -0400

    Fixed Caching Issues for Better Performance. (#381)
    * Aaded caching to write member accessors.
    
    * Removed type member processing if custom serialization is provided.

--- src/ExtendedXmlSerializer/ContentModel/Members/TypeMembers.cs
+++ src/ExtendedXmlSerializer/ContentModel/Members/TypeMembers.cs
@@ -1,33 +1,39 @@
+using ExtendedXmlSerializer.Core.Sources;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using JetBrains.Annotations;
 using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
-using ExtendedXmlSerializer.Core.Sources;
-using JetBrains.Annotations;
 
 namespace ExtendedXmlSerializer.ContentModel.Members
 {
 	sealed class TypeMembers : CacheBase<TypeInfo, ImmutableArray<IMember>>, ITypeMembers
 	{
-		readonly Func<IMember, bool> _specification;
-		readonly ITypeMemberSource   _source;
+		readonly IContainsCustomSerialization _custom;
+		readonly Func<IMember, bool>          _specification;
+		readonly ITypeMemberSource            _source;
 
 		[UsedImplicitly]
-		public TypeMembers(IValidMemberSpecification specification, ITypeMemberSource source)
-			: this(specification.IsSatisfiedBy, source) {}
+		public TypeMembers(IValidMemberSpecification specification, IContainsCustomSerialization custom,
+		                   ITypeMemberSource source) : this(custom, specification.IsSatisfiedBy, source) {}
 
-		public TypeMembers(Func<IMember, bool> specification, ITypeMemberSource source)
+		public TypeMembers(IContainsCustomSerialization custom, Func<IMember, bool> specification,
+		                   ITypeMemberSource source)
 		{
+			_custom        = custom;
 			_specification = specification;
 			_source        = source;
 		}
 
 		protected override ImmutableArray<IMember> Create(TypeInfo parameter)
 		{
-			var result = _source.Get(parameter)
-			                    .Where(_specification)
-			                    .OrderBy(x => x.Order)
-			                    .ToImmutableArray();
+			var result = _custom.IsSatisfiedBy(parameter)
+				             ? ImmutableArray<IMember>.Empty
+				             : _source.Get(parameter)
+				                      .Where(_specification)
+				                      .OrderBy(x => x.Order)
+				                      .ToImmutableArray();
 			return result;
 		}
 	}
---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue375Tests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue375Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			/*var instance = new Subject{ Inner = new Inner() };
+
+			var serializer = new ConfigurationContainer().Create().ForTesting();
+
+			serializer.Serialize(instance);
+
+			serializer.Serialize(instance);*/
+
+		}
+
+		/*sealed class Subject
+		{
+			public Inner Inner { get; set; }
+		}
+
+		sealed class Inner {}*/
+	}
+}
