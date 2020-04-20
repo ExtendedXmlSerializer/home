c58006b13ae7f74ed4b99251b2c0194334acb0f0
Mike-E <michael@dragonspark.network>
2020-04-20 11:57:16 -0400

    Adding support for Flags-based enumerations.

--- src/ExtendedXmlSerializer/ContentModel/Conversion/EnumerationConverters.cs
+++ src/ExtendedXmlSerializer/ContentModel/Conversion/EnumerationConverters.cs
@@ -1,22 +1,41 @@
-using System;
-using System.Reflection;
 using ExtendedXmlSerializer.Core.Sources;
 using ExtendedXmlSerializer.ReflectionModel;
+using System;
+using System.Reflection;
 
 namespace ExtendedXmlSerializer.ContentModel.Conversion
 {
 	sealed class EnumerationConverters : ConditionalSource<TypeInfo, IConverter>, IConverters
 	{
-		public EnumerationConverters(IConverters converters) : base(IsAssignableSpecification<Enum>.Default,
-		                                                            Converters.Default, converters) {}
+		public EnumerationConverters(IConverters converters)
+			: base(IsAssignableSpecification<Enum>.Default, new Converters(converters.Get(typeof(int))), converters) {}
 
 		sealed class Converters : IConverters
 		{
-			public static Converters Default { get; } = new Converters();
+			readonly IConverter _converter;
 
-			Converters() {}
+			public Converters(IConverter converter) => _converter = converter;
 
-			public IConverter Get(TypeInfo parameter) => new EnumerationConverter(parameter.AsType());
+			public IConverter Get(TypeInfo parameter)
+				=> parameter.GetCustomAttribute<FlagsAttribute>() != null
+					   ? new Converter(parameter, _converter).Adapt()
+					   : new EnumerationConverter(parameter.AsType());
+
+			sealed class Converter : ConverterBase<Enum>
+			{
+				readonly Type       _type;
+				readonly IConverter _converter;
+
+				public Converter(Type type, IConverter converter)
+				{
+					_type      = type;
+					_converter = converter;
+				}
+
+				public override Enum Parse(string data) => (Enum)Enum.Parse(_type, data);
+
+				public override string Format(Enum instance) => _converter.Format((int)(object)instance);
+			}
 		}
 	}
 }
---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue386Tests.cs
@@ -0,0 +1,42 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ContentModel.Conversion;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue386Tests
+	{
+		[Fact]
+		void Verify()
+		{
+			var instance = LogMessageSeverity.Debug | LogMessageSeverity.Informational;
+			var serializer = new ConfigurationContainer().Type<LogMessageSeverity>()
+			                                             .Register()
+			                                             .Converter()
+			                                             .Using(Converter.Default)
+			                                             .ForTesting();
+			serializer.Cycle(instance).Should().Be(instance);
+		}
+
+		sealed class Converter : Converter<LogMessageSeverity>
+		{
+			public static Converter Default { get; } = new Converter();
+
+			Converter() : base(x => int.TryParse(x, out var bits)
+				                        ? (LogMessageSeverity)bits
+				                        : LogMessageSeverity.Informational,
+			                   severity => ((int)severity).ToString()) {}
+		}
+
+		[Flags]
+		enum LogMessageSeverity
+		{
+			Debug         = 1,
+			Informational = 2,
+			Critical      = 4
+		}
+	}
+}
