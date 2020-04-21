2b6c2deea3b4f213245207a8efca531c1670a4b0
Mike-E <michael@dragonspark.network>
2020-04-21 03:53:37 -0400

    Passing test for uint.

--- test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue386Tests.cs
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue386Tests.cs
@@ -11,13 +11,29 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 		[Fact]
 		void Verify()
 		{
-			var instance = LogMessageSeverity.Debug | LogMessageSeverity.Informational;
+			var instance   = LogMessageSeverity.Debug | LogMessageSeverity.Informational;
+			var serializer = new ConfigurationContainer().ForTesting();
+			serializer.Cycle(instance).Should().Be(instance);
+		}
+
+		[Fact]
+		void VerifyUnderlying()
+		{
+			var instance   = LogMessageSeverityAlternate.Debug | LogMessageSeverityAlternate.Informational;
 			var serializer = new ConfigurationContainer().ForTesting();
 			serializer.Cycle(instance).Should().Be(instance);
 		}
 
 		[Flags]
 		public enum LogMessageSeverity
+		{
+			Debug         = 1,
+			Informational = 2,
+			Critical      = 4
+		}
+
+		[Flags]
+		public enum LogMessageSeverityAlternate : uint
 		{
 			Debug         = 1,
 			Informational = 2,
