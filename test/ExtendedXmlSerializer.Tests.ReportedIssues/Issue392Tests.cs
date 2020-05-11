81482f499248ce5b9c66a46cde9e271488faae51
Mike-E <michael@dragonspark.network>
2020-05-11 02:31:03 -0400

    Emit initialization times.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue392Tests.cs
@@ -0,0 +1,23 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue392Tests
+	{
+		readonly ITestOutputHelper _output;
+
+		public Issue392Tests(ITestOutputHelper output) => _output = output;
+
+		[Fact]
+		void Verify()
+		{
+			var sw         = Stopwatch.StartNew();
+			var serializer = new ConfigurationContainer().Create();
+			var elapsed    = sw.ElapsedMilliseconds;
+			_output.WriteLine($"{elapsed} ms");
+		}
+	}
+}
