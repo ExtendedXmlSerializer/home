e188e5c149b6557bcce25f3e30aea266deb0b933
Mike-E <michael@dragonspark.network>
2020-06-22 09:47:54 -0400

    Introducing model/test for diagnosing issue.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue407Tests.cs
@@ -0,0 +1,59 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue407Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var type = new PathogenType{ Code = 'f', Description = "Fungus" };
+			var instance = new List<object>
+			{
+				new LookupTable<PathogenDto>
+				{
+					Models = new List<PathogenDto>
+					{
+						new Pathogen{ PathogenType = type }
+					}
+				},
+				new LookupTable<PathogenTypeDto>
+				{
+					Models = new List<PathogenTypeDto> { type }
+				}
+			};
+
+			var serializer = new ConfigurationContainer().EnableReferences().ForTesting();
+			var cycled = serializer.Cycle(instance);
+			cycled[0].To<LookupTable<PathogenDto>>()
+			         .Models.Only()
+			         .PathogenType.Should()
+			         .BeSameAs(cycled[1].To<LookupTable<PathogenTypeDto>>().Models.Only());
+		}
+
+		sealed class LookupTable<T>
+		{
+			public List<T> Models { get; set; } = new List<T>();
+		}
+
+		class PathogenDto
+		{
+			public PathogenTypeDto PathogenType { get; set; }
+		}
+
+		sealed class Pathogen : PathogenDto {}
+
+		class PathogenTypeDto {}
+
+		sealed class PathogenType : PathogenTypeDto
+		{
+			public char Code { get; set; }
+
+			public string Description { get; set; }
+		}
+	}
+}
