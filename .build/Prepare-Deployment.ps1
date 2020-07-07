0bee3de8da09affa256405e75b23a24414e3c3e4
Mike-E <2931376+Mike-E-wins@users.noreply.github.com>
2020-07-07 03:32:01 -0400

    Update Prepare-Deployment.ps1
    (Hopefully) fixing release build breaks.

--- .build/Prepare-Deployment.ps1
+++ .build/Prepare-Deployment.ps1
@@ -4,4 +4,4 @@ Install-Module -Name PowerShellForGitHub
 $env:APPVEYOR_REPO_BRANCH = @{ $true = "master"; $false = $env:APPVEYOR_REPO_BRANCH; }[[bool]$env:APPVEYOR_FORCED_BUILD]
 git checkout $env:APPVEYOR_REPO_BRANCH -q
 
-git submodule update --rebase --remote
+git submodule update --rebase --remote 2>&1
