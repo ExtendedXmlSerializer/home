4661a29fcd3184c5d903e4eff5f4e2e595d5c1c5
Mike-E <michael@dragonspark.network>
2020-07-21 12:09:53 -0400

    Added test for #358.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue358Tests_Extended.cs
@@ -0,0 +1,47 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue358Tests_Extended
+	{
+		[Fact]
+		public void Verify()
+		{
+			var serializer = new ConfigurationContainer().EnableParameterizedContentWithPropertyAssignments()
+			                                             .EnableReferences()
+			                                             .Create()
+			                                             .ForTesting();
+
+			var length   = new length(11);
+			var instance = new vector(length, length);
+
+			var cycled = serializer.Cycle(instance);
+			cycled.L1.Should().BeSameAs(cycled.L2);
+		}
+
+		class length
+		{
+			public length(int value)
+			{
+				Value = value;
+			}
+
+			public int Value { get; }
+		}
+
+		class vector
+		{
+			public vector(length l1, length l2)
+			{
+				L1 = l1;
+				L2 = l2;
+			}
+
+			public length L1 { get; }
+			public length L2 { get; }
+		}
+	}
+}
