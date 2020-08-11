5ddcaa00b5a7879b4035bbadaaf691afb0e968ea
Mike-E <michael@dragonspark.network>
2020-08-11 12:03:52 -0400

    Introduced test for functionality.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue423Tests.cs
@@ -0,0 +1,28 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue423Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var serializer = new ConfigurationContainer().Create().ForTesting();
+			var first = new Subject{ Dictionaries = {new Dictionary<int, string>{[123] = "Hello", [456] = "World"}} };
+			var second = new Subject{ Dictionaries = {new Dictionary<int, string>{[789] = "Hello", [101112] = "Again"}} };
+			var instance = new[] {first, second};
+
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance.Cast<Subject>());
+		}
+
+		sealed class Subject
+		{
+			public List<Dictionary<int, string>> Dictionaries = new List<Dictionary<int, string>>();
+		}
+	}
+}
