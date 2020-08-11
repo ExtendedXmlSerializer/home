147e8a80acc67b0fd50ce383c3ab5d850402abe0
Mike-E <michael@dragonspark.network>
2020-08-11 12:06:15 -0400

    Added property.

--- test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue423Tests.cs
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue423Tests.cs
@@ -1,6 +1,7 @@
 ﻿using ExtendedXmlSerializer.Configuration;
 using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
 using FluentAssertions;
+using JetBrains.Annotations;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -13,8 +14,14 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 		public void Verify()
 		{
 			var serializer = new ConfigurationContainer().Create().ForTesting();
-			var first = new Subject{ Dictionaries = {new Dictionary<int, string>{[123] = "Hello", [456] = "World"}} };
-			var second = new Subject{ Dictionaries = {new Dictionary<int, string>{[789] = "Hello", [101112] = "Again"}} };
+			var first = new Subject
+			{
+				Dictionaries = {new Dictionary<int, string> {[123] = "Hello", [456] = "World"}}
+			};
+			var second = new Subject
+			{
+				Dictionaries = {new Dictionary<int, string> {[789] = "Hello", [101112] = "Again"}}
+			};
 			var instance = new[] {first, second};
 
 			serializer.Cycle(instance).Should().BeEquivalentTo(instance.Cast<Subject>());
@@ -22,7 +29,8 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 
 		sealed class Subject
 		{
-			public List<Dictionary<int, string>> Dictionaries = new List<Dictionary<int, string>>();
+			[UsedImplicitly]
+			public List<Dictionary<int, string>> Dictionaries { get; set; } = new List<Dictionary<int, string>>();
 		}
 	}
 }
