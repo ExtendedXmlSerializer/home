12780c62ba463a248f13dc45973cb74156a2d148
Mike-E <michael@dragonspark.network>
2020-08-20 09:32:00 -0400

    Reverted condition, cleaned up PartitionedTypeSpecification, removed "NotHappy".

--- src/ExtendedXmlSerializer/ContentModel/Reflection/PartitionedTypeSpecification.cs
+++ src/ExtendedXmlSerializer/ContentModel/Reflection/PartitionedTypeSpecification.cs
@@ -1,32 +1,20 @@
-using ExtendedXmlSerializer.Core.Specifications;
 using System.Reflection;
 
 namespace ExtendedXmlSerializer.ContentModel.Reflection
 {
-	sealed class PartitionedTypeSpecification
-		: DecoratedSpecification<TypeInfo>,
-		  IPartitionedTypeSpecification
+	sealed class PartitionedTypeSpecification : IPartitionedTypeSpecification
 	{
 		public static PartitionedTypeSpecification Default { get; } = new PartitionedTypeSpecification();
 
-		PartitionedTypeSpecification() : base(NotHappy.Default) {}
-	}
-
-	sealed class NotHappy : ISpecification<TypeInfo>
-		// HACK: This is a bit of a hack -- ok a total hack, to pass a test for .NET Framework under special conditions: https://github.com/ExtendedXmlSerializer/home/issues/248
-		// This doesn't occur for .NET Core, but we will have to do better for v3 in any case.
-	{
-		public static NotHappy Default { get; } = new NotHappy();
-
-		NotHappy() : this(typeof(object).Assembly) {}
+		PartitionedTypeSpecification() : this(typeof(object).Assembly) {}
 
 		readonly Assembly _assembly;
 
-		public NotHappy(Assembly assembly) => _assembly = assembly;
+		public PartitionedTypeSpecification(Assembly assembly) => _assembly = assembly;
 
+		// HACK: This is a bit of a hack -- ok a total hack, to pass a test for .NET Framework under special conditions:
+		// https://github.com/ExtendedXmlSerializer/home/issues/248
 		public bool IsSatisfiedBy(TypeInfo parameter)
-			=> parameter.Assembly != _assembly
-			   ||
-			   (!parameter.Namespace?.Contains("System.Runtime.Remoting") ?? false);
+			=> parameter.Assembly != _assembly || (!parameter.Namespace?.Contains("System.Runtime.Remoting") ?? true);
 	}
 }
