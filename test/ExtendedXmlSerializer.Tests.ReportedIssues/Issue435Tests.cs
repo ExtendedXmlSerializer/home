81da0607e13191e6d9aa7fbc5ad4cb5117c41a8d
Mike-E <michael@dragonspark.network>
2020-08-28 12:01:28 -0400

    Added test demonstrating basic serialization/deserialization via EnableParameterizedContent.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue435Tests.cs
@@ -0,0 +1,31 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue435Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var instance = new Range<int>(1, 2);
+			var serializer = new ConfigurationContainer().Type<Range<int>>().EnableParameterizedContent().Create();
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		public readonly struct Range<T>
+		{
+			public Range(T min, T max)
+			{
+				Min = min;
+				Max = max;
+			}
+
+			public T Min { get; }
+
+			public T Max { get; }
+		}
+	}
+}
