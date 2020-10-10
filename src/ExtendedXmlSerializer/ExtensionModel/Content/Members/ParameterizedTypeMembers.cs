6cc226ed69be92780b2945b4bc5969d7b38e0702
Mike-E <michael@dragonspark.network>
2020-10-10 09:54:33 -0400

    Skipped processing of parameterized-content members that have custom serializers applied.

--- src/ExtendedXmlSerializer/ExtensionModel/Content/Members/ParameterizedTypeMembers.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Content/Members/ParameterizedTypeMembers.cs
@@ -1,29 +1,44 @@
+using ExtendedXmlSerializer.ContentModel.Identification;
+using ExtendedXmlSerializer.ContentModel.Members;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
+using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
-using ExtendedXmlSerializer.ContentModel.Identification;
-using ExtendedXmlSerializer.ContentModel.Members;
 
 namespace ExtendedXmlSerializer.ExtensionModel.Content.Members
 {
 	sealed class ParameterizedTypeMembers : ITypeMembers
 	{
-		readonly ITypeMembers          _typed;
-		readonly IParameterizedMembers _members;
+		readonly ITypeMembers                 _previous;
+		readonly IContainsCustomSerialization _custom;
+		readonly IParameterizedMembers        _members;
+		readonly Func<IMember, string>        _group;
 
-		public ParameterizedTypeMembers(ITypeMembers typed, IParameterizedMembers members)
+		public ParameterizedTypeMembers(ITypeMembers previous, IContainsCustomSerialization custom,
+		                                IParameterizedMembers members)
+			: this(previous, custom, members, IdentityFormatter.Default.Get) {}
+
+		// ReSharper disable once TooManyDependencies
+		public ParameterizedTypeMembers(ITypeMembers previous, IContainsCustomSerialization custom,
+		                                IParameterizedMembers members, Func<IMember, string> group)
 		{
-			_typed   = typed;
-			_members = members;
+			_previous = previous;
+			_custom   = custom;
+			_members  = members;
+			_group    = @group;
 		}
 
 		public ImmutableArray<IMember> Get(TypeInfo parameter)
+			=> _custom.IsSatisfiedBy(parameter) ? ImmutableArray<IMember>.Empty : Implementation(parameter);
+
+		ImmutableArray<IMember> Implementation(TypeInfo parameter)
 		{
 			var members = _members.Get(parameter);
-			var typed   = _typed.Get(parameter);
+			var typed   = _previous.Get(parameter);
 			var items   = members?.AddRange(typed) ?? typed;
 
-			var result = items.GroupBy(IdentityFormatter.Default.Get)
+			var result = items.GroupBy(_group)
 			                  .Select(x => x.OfType<ParameterizedMember>()
 			                                .FirstOrDefault() ?? x.First())
 			                  .ToImmutableArray();
---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue458Tests.cs
@@ -0,0 +1,53 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ContentModel;
+using ExtendedXmlSerializer.ContentModel.Format;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using System;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue458Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var instance = new DecoratedImage(null);
+
+			var serializer = new ConfigurationContainer().EnableParameterizedContentWithPropertyAssignments()
+			                                             .UseOptimizedNamespaces()
+														 //
+			                                             .Type<DecoratedImage>()
+			                                             .Register()
+			                                             .Serializer()
+			                                             .Of<ImageSerializer>()
+														 //
+			                                             .Create()
+			                                             .ForTesting();
+
+			serializer.Assert(instance, @"<?xml version=""1.0"" encoding=""utf-8""?><Issue458Tests-DecoratedImage xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests.ReportedIssues"" />");
+		}
+
+		public class ImageSerializer : ISerializer<DecoratedImage>
+		{
+			public DecoratedImage Get(IFormatReader parameter) => new DecoratedImage(null);
+
+			public void Write(IFormatWriter writer, DecoratedImage instance) {}
+		}
+
+		public class DecoratedImage
+		{
+			public DecoratedImage(Metadata metadata) => Metadata = metadata;
+
+			public Metadata Metadata
+			{
+				// ReSharper disable once ThrowExceptionInUnexpectedLocation
+				get => throw new NotImplementedException();
+				// ReSharper disable once RedundantAssignment
+				set => value = null;
+			}
+		}
+
+		public class Metadata {}
+	}
+}
