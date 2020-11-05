8d0efb61091ae3623b56b20ad347a0a2d6fe310f
Mike-E <michael@dragonspark.network>
2020-11-05 09:19:21 -0500

    Additional formatting.

--- test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue475Tests.cs
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue475Tests.cs
@@ -10,14 +10,16 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 		[Fact]
 		public void Verify()
 		{
-			var serializer = new ConfigurationContainer().Type<Subject>().Member(x => x.NoMessage).Ignore().Create();
+			var serializer = new ConfigurationContainer().Type<Subject>()
+			                                             .Member(x => x.NoMessage)
+			                                             .Ignore()
+			                                             .Create();
 
 			var instance = new Subject { Message = "Hello World!", NoMessage = "Ignored" };
 
 			var cycled = serializer.Cycle(instance);
 			cycled.NoMessage.Should().BeNullOrEmpty();
 			cycled.Message.Should().Be(instance.Message);
-
 		}
 
 		sealed class Subject
