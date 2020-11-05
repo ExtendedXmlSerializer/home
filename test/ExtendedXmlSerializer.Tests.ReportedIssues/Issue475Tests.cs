de270eb5706b5cabf761ea0037ab737ca02e0acb
Mike-E <michael@dragonspark.network>
2020-11-05 07:45:44 -0500

    Demonstration of `Ignore` method.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue475Tests.cs
@@ -0,0 +1,30 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue475Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var serializer = new ConfigurationContainer().Type<Subject>().Member(x => x.NoMessage).Ignore().Create();
+
+			var instance = new Subject { Message = "Hello World!", NoMessage = "Ignored" };
+
+			var cycled = serializer.Cycle(instance);
+			cycled.NoMessage.Should().BeNullOrEmpty();
+			cycled.Message.Should().Be(instance.Message);
+
+		}
+
+		sealed class Subject
+		{
+			public string Message { get; set; }
+
+			public string NoMessage { get; set; }
+		}
+	}
+}
