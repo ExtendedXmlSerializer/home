3940f0b99ccfc2e2da96e746add1beb768d40749
Mike-E <michael@dragonspark.network>
2020-11-21 05:05:42 -0500

    Test CI Template Message

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue479Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue479Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			true.Should().BeTrue();
+		}
+	}
+}
