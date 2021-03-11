cd0cd55be4ae08a4f51e525e55a4f096ea6f170b
Mike-E <michael@dragonspark.network>
2021-03-11 09:03:22 -0500

    Basic demonstration of exception configuration

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue507Tests.cs
@@ -0,0 +1,36 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Xml;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue507Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var serializer = new ConfigurationContainer().EnableMemberExceptionHandling()
+			                                             .WithUnknownContent()
+			                                             .Throw()
+			                                             .Create()
+			                                             .ForTesting();
+			var document = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Issue507Tests-Subject xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests.ReportedIssues"">
+	<Message>hello world!</Message>
+	<DoesNotExist>This is very broken</DoesNotExist>
+</Issue507Tests-Subject>";
+			serializer.Invoking(x => x.Deserialize<Subject>(document))
+			          .Should()
+			          .Throw<XmlException>()
+			          .Which.LinePosition.Should()
+			          .BeGreaterThan(0);
+		}
+
+		sealed class Subject
+		{
+			public string Message { get; set; }
+		}
+	}
+}
