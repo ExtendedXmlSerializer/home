09a649849529d0ae42147cf84b16776f8e402833
Mike-E <michael@dragonspark.network>
2021-05-19 03:31:10 -0400

    Fixed `WithUnknownContent().Continue` to account for complex content

--- src/ExtendedXmlSerializer/ExtensionModel/Content/ContinueOnUnknownContent.cs
+++ src/ExtendedXmlSerializer/ExtensionModel/Content/ContinueOnUnknownContent.cs
@@ -1,5 +1,6 @@
 ﻿using ExtendedXmlSerializer.ContentModel.Format;
 using ExtendedXmlSerializer.Core;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
 
 namespace ExtendedXmlSerializer.ExtensionModel.Content
 {
@@ -11,7 +12,19 @@ namespace ExtendedXmlSerializer.ExtensionModel.Content
 
 		public void Execute(IFormatReader parameter)
 		{
+			var reader = parameter.Get().To<System.Xml.XmlReader>();
+			var depth  = XmlDepth.Default.Get(reader);
+
 			parameter.Content();
+
+			if (depth.HasValue)
+			{
+				while (XmlDepth.Default.Get(reader) > depth)
+				{
+					reader.Skip();
+					parameter.Set();
+				}
+			}
 		}
 	}
 }
