84934919adf04d03457a6702e330eaa02c99ca12
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2021-08-29 08:09:27 -0400

    Accounted for empty content for nullable attributes (#531)

--- test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue530Tests.cs
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue530Tests.cs
@@ -11,17 +11,67 @@ namespace ExtendedXmlSerializer.Tests.ReportedIssues
 		[Fact]
 		public void Verify()
 		{
-			var instance = new Subject{ List = new List<string>() };
+			var instance = new Subject { List = new List<string>() };
 
 			var container = new ConfigurationContainer().Emit(EmitBehaviors.Classic).Create().ForTesting();
 
 			container.Cycle(instance).Should().BeEquivalentTo(instance);
 		}
 
+		[Fact]
+		public void VerifyNotEmit()
+		{
+			var instance = new EmitMe()
+			{
+				Message    = "This is emitted",
+				NotEmitted = new DoNotEmitMe { Message = "But I want to be emitted... :( :( :(" }
+			};
+
+			var container = new ConfigurationContainer().Type<DoNotEmitMe>()
+			                                            .EmitWhen(_ => false)
+			                                            .Member(x => x.Message)
+			                                            .EmitWhen(x => x.Contains("emitted"))
+			                                            .Create()
+			                                            .ForTesting();
+			container.Assert(instance,
+			                 @"<?xml version=""1.0"" encoding=""utf-8""?><Issue530Tests-EmitMe xmlns=""clr-namespace:ExtendedXmlSerializer.Tests.ReportedIssues;assembly=ExtendedXmlSerializer.Tests.ReportedIssues""><Message>This is emitted</Message></Issue530Tests-EmitMe>");
+		}
+
+		[Fact]
+		public void VerifyNullable()
+		{
+			var instance = new TestClass { ValueList = new List<double>() };
+
+			var sut = new ConfigurationContainer().UseAutoFormatting()
+			                                      .Emit(EmitBehaviors.Always)
+			                                      .Create()
+			                                      .ForTesting();
+
+			sut.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		public class TestClass
+		{
+			public double? Value1;
+
+			public List<double> ValueList;
+		}
 
 		sealed class Subject
 		{
 			public List<string> List { get; set; }
+		}
+
+		sealed class EmitMe
+		{
+			public string Message { get; set; }
+
+			public DoNotEmitMe NotEmitted { get; set; }
+		}
+
+		sealed class DoNotEmitMe
+		{
+			public string Message { get; set; }
 		}
 	}
 }
