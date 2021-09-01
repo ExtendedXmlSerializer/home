16d24df8993790c30572b82f0605169be56c39fc
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2021-09-01 18:33:10 -0400

    Added hypen as a valid character for assembly names (#544)
    * Added hypen as a valid character for assembly names
    
    * Added test w/ namespace

---
+++ test/ExtendedXmlSerializer.Tests/ReportedIssues/Issue543Tests.cs
@@ -0,0 +1,31 @@
+﻿using ExtendedXmlSerializer.Core.Parsing;
+using FluentAssertions;
+using Sprache;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue543Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			const string input = "clr-namespace:;assembly=Some-Assembly.Tests";
+			var path = ExtendedXmlSerializer.ContentModel.Reflection.AssemblyPathParser.Default.ToParser()
+			                                .Parse(input);
+			path.Namespace.Should().BeNull();
+			path.Path.Should().Be("Some-Assembly.Tests");
+		}
+
+		[Fact]
+		public void VerifyWithNamespace()
+		{
+			const string input = "clr-namespace:Some.Namespace.Here;assembly=Some-Assembly.Tests";
+			var path = ExtendedXmlSerializer.ContentModel.Reflection.AssemblyPathParser.Default.ToParser()
+			                                .Parse(input);
+			path.Namespace.Should().Be("Some.Namespace.Here");
+			path.Path.Should().Be("Some-Assembly.Tests");
+		}
+
+	}
+}
