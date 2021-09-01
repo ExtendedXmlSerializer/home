5e1cce9a6926e42b25a786149624e170d7b6668e
Mike-E <michael@dragonspark.network>
2021-09-01 10:22:00 -0400

    Fixed problem with namespace not emitting properly for optimized namespace scenarios.

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue545Tests.cs
@@ -0,0 +1,73 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue545Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var instance = new ClassWithDictionary();
+			instance.Dict.Add("key",123);
+
+			var configuration = new ConfigurationContainer();
+			var serializer    = configuration.Create().ForTesting();
+
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		[Fact]
+		public void VerifyImplicitTyping()
+		{
+			var instance = new ClassWithDictionary();
+			instance.Dict.Add("key",123);
+
+			var configuration = new ConfigurationContainer().EnableImplicitTyping(typeof(ClassWithDictionary));
+			var serializer    = configuration.Create().ForTesting();
+
+			//serializer.WriteLine(instance);
+
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		[Fact]
+		public void VerifyOptimizedImplicitTyping()
+		{
+			var instance = new ClassWithDictionary();
+			instance.Dict.Add("key",123);
+
+			var configuration = new ConfigurationContainer().UseOptimizedNamespaces().EnableImplicitTyping(typeof(ClassWithDictionary));
+			var serializer    = configuration.Create().ForTesting();
+
+			//serializer.WriteLine(instance);
+
+			serializer.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+
+		public class ClassWithDictionary
+		{
+			public ClassWithDictionary()
+			{
+				Dict = new Dictionary<string, object>();
+
+				ListWithItems = new List<ClassWithProperty>();
+				ListWithItems.Add(new ClassWithProperty());
+			}
+
+			public Dictionary<string,object> Dict { get; set; }
+
+			public IList<ClassWithProperty> ListWithItems { get; set; }
+		}
+
+		public class ClassWithProperty
+		{
+			public string Text { get; set; }
+		}
+
+	}
+}
