775a5c9e7a3cbe036c83ee7729654db0fed14462
Mike-E <michael@dragonspark.network>
2021-11-02 11:08:20 -0400

    Quick sanity check to ensure implicit double can be serialized

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue561Tests.cs
@@ -0,0 +1,19 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue561Tests
+	{
+		[Fact]
+		public void Verify()
+		{
+			var subject  = new ConfigurationContainer().EnableImplicitTyping(typeof(double)).Create().ForTesting();
+			const double instance = 123d;
+			subject.Assert(instance, @"<?xml version=""1.0"" encoding=""utf-8""?><double>123</double>");
+			subject.Cycle(instance).Should().Be(instance);
+		}
+	}
+}
