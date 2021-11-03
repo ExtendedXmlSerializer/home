cdf14979034f523267968a0446f5f3d07390863a
Mike-E <michael@dragonspark.network>
2021-11-03 13:37:47 -0400

    Comparer example

---
+++ test/ExtendedXmlSerializer.Tests.ReportedIssues/Issue565Tests_Extended.cs
@@ -0,0 +1,40 @@
+﻿using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.Tests.ReportedIssues.Support;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtendedXmlSerializer.Tests.ReportedIssues
+{
+	public sealed class Issue565Tests_Extended
+	{
+		[Fact]
+		public void Verify()
+		{
+			var subject  = new ConfigurationContainer().Create().ForTesting();
+			var instance = new Store
+			{
+				[new byte[2]] = "First",
+				[new byte[3]] = "Second",
+				[new byte[4]] = "Third"
+			};
+			subject.Cycle(instance).Should().BeEquivalentTo(instance);
+		}
+
+		sealed class Store : Dictionary<byte[], string>
+		{
+			public Store() : base(Issue565Tests_Extended.Comparer.Instance) {}
+		}
+
+		sealed class Comparer : EqualityComparer<byte[]>
+		{
+			public static Comparer Instance { get; } = new();
+
+			Comparer() {}
+
+			public override bool Equals(byte[] x, byte[] y) => x.Length == y.Length;
+
+			public override int GetHashCode(byte[] obj) => obj.Length.GetHashCode();
+		}
+	}
+}
